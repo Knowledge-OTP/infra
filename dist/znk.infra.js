2b677dcafe245a0a185f4dd71563d20444745c31
Oded <oded.sharir@gmail.com>
2015-12-08 14:30:45 +0200

    new infra

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -41,6 +41,8 @@
 
             var contentFunc;
 
+            var contentDataFunc = _getContentData();
+
             var revSrv = {};
 
             function _getContentFunc(){
@@ -50,36 +52,60 @@
                 return contentFunc;
             }
 
+            function _getContentData() {
+                var contentData;
+                return function() {
+                     return {
+                         get: function() {
+                             if(!contentData) {
+                                 return _getContentFunc().then(function(dataObj) {
+                                     contentData = dataObj;
+                                     return dataObj;
+                                 });
+                             }
+                             return $q.when(contentData);
+                         },
+                         set: function(practiceName, newData) {
+                             contentData.revisionManifest[practiceName] = newData;
+                             return $q.when({ rev: newData.rev, status: 'update'});
+                         }
+                     }
+                }
+            }
+
             revSrv.getRev = function(practiceName) {
-                return _getContentFunc().then(function(dataObj) {
+                return contentDataFunc().get().then(function(dataObj) {
 
                     if(!dataObj || !dataObj.revisionManifest || !dataObj.latestRevisions) {
-                        return { error: 'No Data Found! ' +dataObj };
+                        return { error: 'No Data Found! ', data: dataObj };
                     }
 
-                    var userManifest = dataObj.latestRevisions[practiceName];
-                    var publicationManifest = dataObj.revisionManifest[practiceName];
+                    var userManifest = dataObj.revisionManifest[practiceName];
+                    var publicationManifest = dataObj.latestRevisions[practiceName];
                     var newRev;
 
                     if(angular.isUndefined(publicationManifest)) {
-                        return { error: 'Not Found' };
+                        return { error: 'Not Found', data: dataObj };
                     }
 
                     if(!userManifest) {
                         newRev = { rev:  publicationManifest.rev, status: 'new' };
-                        dataObj.latestRevisions[practiceName] = { rev: newRev.rev };
                     } else if(userManifest.rev < publicationManifest.rev) {
                         newRev = { rev:  userManifest.rev, status: 'old' };
                     } else if(userManifest.rev === publicationManifest.rev) {
                         newRev = { rev:  publicationManifest.rev, status: 'same' };
                     } else {
-                        newRev = { error: 'failed to get revision!' };
+                        newRev = { error: 'failed to get revision!', data: dataObj };
                     }
 
 
                     return newRev;
 
                 });
+            };
+
+            revSrv.setRev = function(practiceName, newRev) {
+                return contentDataFunc().set(practiceName, { rev: newRev });
             };
 
             return revSrv;
