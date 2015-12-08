c2f0584e21a0ff3fe310bb53a8aff61f13beede7
Oded <oded.sharir@gmail.com>
2015-12-08 10:56:52 +0200

    add as dep

--- src/core/config.js
+++ src/core/config.js
@@ -1,5 +1,5 @@
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra', ['znk.infra.pngSequence', 'znk.infra.enum', 'znk.infra.svgIcon']);
+    angular.module('znk.infra', ['znk.infra.pngSequence', 'znk.infra.enum', 'znk.infra.svgIcon', 'znk.infra.content']);
 })(angular);
