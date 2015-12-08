0ea4d4eb1ba4f7b420b0943b73315ebec8f8a9b6
igormas <igor.masliansky@gmail.com>
2015-12-08 10:33:06 +0200

    Subject enum

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -42,6 +42,36 @@
         }
     ]);
 })(angular);
+(function (angular) {
+    'use strict';
+
+    var subjectEnum = {
+        MATH: 0,
+        READING: 1,
+        WRITING: 2,
+        LISTENING: 3,
+        SPEAKING: 4
+    };
+
+    angular.module('znk.infra.enum').constant('SubjectEnumConst', subjectEnum);
+
+    angular.module('znk.infra.enum').factory('SubjectEnum', [
+        'EnumSrv',
+        function (EnumSrv) {
+
+            var SubjectEnum = new EnumSrv.BaseEnum([
+                ['MATH', subjectEnum.MATH, 'math'],
+                ['READING', subjectEnum.READING, 'reading'],
+                ['WRITING', subjectEnum.WRITING, 'writing'],
+                ['LISTENING', subjectEnum.LISTENING, 'listening'],
+                ['SPEAKING', subjectEnum.SPEAKING, 'speaking']
+            ]);
+
+            return SubjectEnum;
+        }
+    ]);
+})(angular);
+
 'use strict';
 (function (angular) {
     angular.module('znk.infra.enum').factory('EnumSrv', [
