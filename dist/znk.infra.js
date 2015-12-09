01fa6f80fc7d3a4274d6f56e09d16a2b5b9d6882
igormas <igor.masliansky@gmail.com>
2015-12-09 14:04:22 +0200

    max left and right scrolling were added.

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -313,9 +313,9 @@
     'use strict';
 
     angular.module('znk.infra.scroll').directive('znkScroll', [
-        '$log',
-        function ($log) {
-            function setElementTranslateX(element,val,isOffset){
+        '$log', '$window',
+        function ($log, $window) {
+            function setElementTranslateX(element,val,isOffset,minVal,maxVal){
                 var domElement = angular.isArray(element) ? element[0] : element;
                 var newTranslateX = val;
                 if(isOffset){
@@ -330,6 +330,12 @@
                     }
                     newTranslateX += currX;
                 }
+                minVal = angular.isUndefined(minVal) ? -Infinity : minVal;
+                maxVal = angular.isUndefined(maxVal) ? Infinity : maxVal;
+
+                newTranslateX = Math.max(newTranslateX,minVal);
+                newTranslateX = Math.min(newTranslateX,maxVal);
+
                 var newTransformValue = 'translateX(' + newTranslateX + 'px)';
                 setCssPropery(domElement,'transform',newTransformValue);
             }
@@ -350,6 +356,14 @@
                     function mouseDownHandler(evt){
                         $log.debug('mouse down',evt.pageX);
 
+                        var containerWidth = domElement.offsetWidth;
+
+                        var child = domElement.children[0];
+                        var childStyle = $window.getComputedStyle(child);
+                        var childMarginRight = +childStyle.marginRight.replace('px','');
+                        var childMarginLeft = +childStyle.marginLeft.replace('px','');
+                        var childWidth = child.offsetWidth + childMarginLeft + childMarginRight;
+
                         var currMousePoint = {
                             x: evt.pageX,
                             y: evt.pageY
@@ -362,7 +376,7 @@
 
                             currMousePoint.x = evt.pageX;
                             currMousePoint.y = evt.pageY;
-                            moveScroll(xOffset);
+                            moveScroll(containerWidth,childWidth,xOffset);
                         }
                         document.addEventListener('mousemove',mouseMoveEventHandler);
 
@@ -374,15 +388,18 @@
                         document.addEventListener('mouseup',mouseUpEventHandler);
                     }
 
-                    function moveScroll(xOffset/*,yOffset*/){
+                    function moveScroll(containerWidth,childWidth,xOffset/*,yOffset*/){
                         var children = domElement.children;
                         var child = children[0];
 
+                        var minTranslateX = Math.min(containerWidth - childWidth,0);
+                        var maxTranslateX = 0;
+
                         if(!child.style.transform){
-                            setElementTranslateX(child,0);
+                            setElementTranslateX(child,0,false,false,minTranslateX,maxTranslateX);
                         }
 
-                        setElementTranslateX(child,xOffset,true);
+                        setElementTranslateX(child,xOffset,true,minTranslateX,maxTranslateX);
                     }
 
                     function preFn(scope,element,attrs){
