40562e8b721354aa6eabe530e02b0c9b901fbf4e
kosta <kosta.masliansky@gmail.com>
2015-12-24 11:47:09 +0200

    exercise time was added

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -203,6 +203,22 @@
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.enum').factory('ExerciseTimeEnum', [
+        'EnumSrv',
+        function (EnumSrv) {
+            return new EnumSrv.BaseEnum([
+                ['5_MIN', 5, '5 min'],
+                ['10_MIN', 10, '10 min'],
+                ['15_MIN', 15, '15 min']
+            ]);
+        }
+    ]);
+})(angular);
+
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra.enum').factory('ExerciseTypeEnum', [
         'EnumSrv',
         function (EnumSrv) {
