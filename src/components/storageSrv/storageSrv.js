731805e139c08b9fe54eb07c9228bad2452c17bc
igormas <igor.masliansky@gmail.com>
2016-01-10 17:26:01 +0200

    pr fixes

--- src/components/storageSrv/storageSrv.js
+++ src/components/storageSrv/storageSrv.js
@@ -63,31 +63,6 @@
                 return this.setter(this.path, entity);
             };
 
-            //function processPath(path) {
-            //    var processedPath = path.replace(/\$\$uid/, authObj.uid);
-            //    return processedPath;
-            //}
-            //
-            //function entityGetter(path) {
-            //    var processedPath = processPath(path);
-            //    return $firebaseObject(new Firebase(ENV.fbDataEndPoint + processedPath)).$loaded();
-            //}
-            //
-            //function entitySetter(path, newEntity) {
-            //    if (newEntity.$save) {
-            //        return newEntity.$save();
-            //    }
-            //    return entityGetter(path).then(function (sourceEntity) {
-            //        if (!angular.isObject(newEntity)) {
-            //            var fallbackObj = {};
-            //            fallbackObj[newEntity] = newEntity;
-            //            newEntity = fallbackObj;
-            //        }
-            //        angular.extend(sourceEntity, newEntity);
-            //        return sourceEntity.$save();
-            //    });
-            //}
-
             function StorageSrv(entityGetter, entitySetter) {
                 //var authObj = $firebaseAuth(new Firebase(ENV.fbDataEndPoint)).$getAuth();
 
@@ -96,21 +71,6 @@
                 this.EntityCommunicator = function (path, defaultValues) {
                     return new EntityCommunicator(path, defaultValues, entityGetter, entitySetter);
                 };
-
-                //this.createGuid = function () {
-                //    function s4() {
-                //        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1); // jshint ignore:line
-                //    }
-                //
-                //    return (s4() + s4() + '-' + s4() + '-4' + s4().substr(0, 3) + '-' + s4() + '-' + s4() + s4() + s4()).toLowerCase();
-                //};
-                //
-                //this.variables = {
-                //    uid: '$$uid',
-                //    appPath: applicationPath,
-                //    appUserSpacePath: applicationPath + '/users/' + authObj.uid
-                //};
-
             }
 
             return StorageSrv;
