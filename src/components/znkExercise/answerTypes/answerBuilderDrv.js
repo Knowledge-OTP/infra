437b2c0502cd225ddff2138e34c0a3e063966d06
igormas <igor.masliansky@gmail.com>
2016-01-13 18:24:17 +0200

    render function was added

--- src/components/znkExercise/answerTypes/answerBuilderDrv.js
+++ src/components/znkExercise/answerTypes/answerBuilderDrv.js
@@ -6,8 +6,8 @@
     'use strict';
 
     angular.module('znk.infra.znkExercise').directive('answerBuilder', [
-        '$compile', 'AnswerTypeEnum', 'ZnkExerciseUtilitySrv',
-        function ($compile, AnswerTypeEnum, ZnkExerciseUtilitySrv) {
+        '$compile', 'AnswerTypeEnum', 'ZnkExerciseUtilitySrv', 'ZnkExerciseViewModeEnum',
+        function ($compile, AnswerTypeEnum, ZnkExerciseUtilitySrv, ZnkExerciseViewModeEnum) {
             var typeToViewMap = {};
 
             typeToViewMap[AnswerTypeEnum.SELECT_ANSWER.enum] = '<select-answer></select-answer>';
@@ -15,7 +15,7 @@
             typeToViewMap[AnswerTypeEnum.RATE_ANSWER.enum] = '<rate-answer></rate-answer>';
 
             return {
-                require: ['answerBuilder','^questionBuilder'],
+                require: ['answerBuilder','^questionBuilder', '^ngModel'],
                 restrict: 'E',
                 controller:[
                     function(){
@@ -26,9 +26,18 @@
                     pre:function (scope, element, attrs, ctrls) {
                         var answerBuilderCtrl = ctrls[0];
                         var questionBuilderCtrl = ctrls[1];
+                        var ngModelCtrl = ctrls[2];
 
                         var fnToBindFromQuestionBuilder = ['getViewMode'];
                         ZnkExerciseUtilitySrv.bindFunctions(answerBuilderCtrl,questionBuilderCtrl,fnToBindFromQuestionBuilder);
+
+                        answerBuilderCtrl.canUserAnswerBeChanged = function(){
+                            var viewMode = questionBuilderCtrl.getViewMode();
+                            var isntReviewMode = viewMode !== ZnkExerciseViewModeEnum.REVIEW.enum;
+                            var notAnswered = angular.isDefined(ngModelCtrl.$viewValue);
+                            var isAnswerWithResultViewMode = viewMode === ZnkExerciseViewModeEnum.ANSWER_WITH_RESULT.enum;
+                            return isntReviewMode && isAnswerWithResultViewMode && notAnswered;
+                        };
 
                         answerBuilderCtrl.question = questionBuilderCtrl.question;
 
