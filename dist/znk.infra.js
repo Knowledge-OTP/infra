fe3d6c11d648e4b3e4f722c2cda4d363e4f6d578
igormas <igor.masliansky@gmail.com>
2016-01-19 11:22:00 +0200

    v10 rc

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -248,6 +248,20 @@
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.enum').factory('ExamTypeEnum', [
+        'EnumSrv',
+        function (EnumSrv) {
+            return new EnumSrv.BaseEnum([
+                ['FULL TEST', 0, 'test'],
+                ['MINI TEST', 1, 'miniTest']]);
+        }
+    ]);
+})(angular);
+
+
+(function (angular) {
+    'use strict';
+
     var exerciseStatusEnum = {
         NEW: 0,
         ACTIVE: 1,
