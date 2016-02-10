558a25b6dd375ecb1370e535b1bff2f1121da464
igormas <igor.masliansky@gmail.com>
2016-02-10 12:42:19 +0200

    pr fixes

--- test/jsonFixtures/config.js
+++ test/jsonFixtures/config.js
@@ -2,7 +2,18 @@
     jasmine.getFixtures().fixturesPath = "base/test/jsonFixtures/";
 
     window.content = JSON.parse(readFixtures("content.json"));
-    window.freeContent  = JSON.parse(readFixtures("freeContent.json"));
-    window.iap = JSON.parse(readFixtures("iap.json"));
-    window.abTesting = JSON.parse(readFixtures("abTesting.json"));
+
+/*  cleaning content
+    var exercise = content.;
+    exercise.questionsGroupData.forEach(function(question){if(!question.paragraphs)return;question.paragraphs.forEach(function(paragraph){paragraph.body='body'})})
+
+    exercise.questions.forEach(function(question){if(!question.paragraphs)return;question.paragraphs.forEach(function(paragraph){paragraph.body='body'})})
+
+    exercise.questions.forEach(function(question){question.paragraph="paragraph"})
+
+    exercise.questions.forEach(function(question){question.content= 'content for question id:' + question.id})
+
+    exercise.questions.forEach(function(question){question.writtenSln= 'written solution for question id:' + question.id})
+
+    exercise.questions.forEach(function(question){question.answers.forEach(function(answer){answer.content='content'})})*/
 })();
