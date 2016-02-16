1187ccc1c50eaabb3fee41a07f8f5e8e9d091eb2
ramizi <rami@zinkerz.com>
2016-02-16 16:31:59 +0200

    making sure the video src attribute can be retrieve from angular expression

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -1661,8 +1661,15 @@
  *      videoNotAvail:
  *         the message to display when video is not available
  *         example : video-not-avail="<div class=&quot;video_not_available&quot; ></div>">
+ *         vidPoster:
+ *         the poster for the video.  we dynamically check it . that is why we don't use the
+ *         default poster attribute
  *      defaultPoster:
  *          the default poster to display when poster is unavailable or is empty
+ *      ng-src
+ *          the source for the vid
+ *          example : $scope.videoUrl = $sce.trustAsResourceUrl(
+ 'http://d1qqqwawt7o27r.cloudfront.net/videos/questions/1768.mp4');
  */
 'use strict';
 angular.module('znk.infra.general')
@@ -1670,28 +1677,32 @@ angular.module('znk.infra.general')
            return {
                link: function (scope, element, attrs) {
                    var vidElm = element[0];
-                   var sources = vidElm.querySelectorAll('source');
                    var vidNotAvail = (angular.isDefined(attrs.videoNotAvail)) ?
                        attrs.videoNotAvail : '<div class=\'video_not_available\'></div>';
-                   var setVid = function (videoTag) {
+                   var setPoster = function (videoTag) {
                        videoTag.poster =
                            (angular.isDefined(attrs.defaultPoster)) ? attrs.defaultPoster :
                                'http://corrupt-system.de/assets/media/sintel/sintel-trailer.jpg';
                    };
-                   if (angular.isUndefined(attrs.poster) || attrs.poster === '') {
-                       setVid(vidElm);
+                   var setVid = function (elmCur, vidNotAvail) {
+                       elmCur.replaceWith(vidNotAvail);
+                   };
+                   if (angular.isUndefined(attrs.vidPoster) || attrs.vidPoster === '') {
+                       setPoster(vidElm);
                    }
                    else {
                        var image = new Image();
-                       image.src = attrs.poster;
+                       image.src = attrs.vidPoster;
                        image.onerror = function () {
-                           setVid(vidElm);
+                           setPoster(vidElm);
                        };
                    }
-                   if (sources.length !== 0) {
-                       var lastSource = sources[sources.length - 1];
-                       lastSource.addEventListener('error', function () {
-                           element.replaceWith(vidNotAvail);
+                   if (angular.isUndefined(attrs.ngSrc) || attrs.ngSrc === '') {
+                       setVid(element, vidNotAvail);
+                   }
+                   else {
+                       vidElm.addEventListener('error', function () {
+                           setVid(element, vidNotAvail);
                        });
                        vidElm.addEventListener('loadeddata', function () {
                            this.style.visibility = 'visible';
