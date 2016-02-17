dac5c0a61f9a9e1db6c34e0e47e9f8620648ee5f
ramizi <rami@zinkerz.com>
2016-02-17 15:09:37 +0200

    change setVid name to replaceVIdTag

--- demo/videoCtrlDrv/demoApp.js
+++ demo/videoCtrlDrv/demoApp.js
@@ -6,9 +6,8 @@
                $scope.vidNotAvail = '<div class=\'video_not_available\' ></div>';
                $scope.poster ='http://corrupt-system.de/assets/media/sintel/sintel-trailer.jp';
                $scope.defaultPoster='http://corrupt-system.de/assets/media/sintel/sintel-trailer.jpg';
-               //$scope.videoUrl = $sce.trustAsResourceUrl(
-               //    'http://d1qqqwawt7o27r.cloudfront.net/videos/questions/1768.mp4');
-               $scope.videoUrl = undefined;
+               $scope.videoUrl = $sce.trustAsResourceUrl(
+                   'http://d1qqqwawt7o27r.cloudfront.net/videos/questions/1768.mp4');
            }]);
 
 })(angular);
