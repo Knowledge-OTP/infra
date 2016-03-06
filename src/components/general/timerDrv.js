ebb40adb0fa30bd1d2b46ce72ea5f2bd1cb3a627
stormcrow85 <ran.shternin@gmail.com>
2016-03-06 16:59:08 +0200

    added attribute to stop timer on zero (optional)

--- src/components/general/timerDrv.js
+++ src/components/general/timerDrv.js
@@ -31,7 +31,7 @@
 
             return {
                 scope: {
-                    play: '&',
+                    play: '=',
                     typeGetter: '&?type',
                     configGetter: '&?config'
                 },
@@ -116,7 +116,7 @@
                         currentTime += scope.config.countDown ? -INTERVAL_TIME : INTERVAL_TIME;
 
                         if(scope.config.stopOnZero && currentTime === 0){
-                            return;
+                            return scope.play === false;
                         }
 
                         updateTime(currentTime);
