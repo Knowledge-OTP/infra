1756b2c5d13fad9d020be212526baf16b39c6da1
igormas <igor.masliansky@gmail.com>
2016-03-08 17:47:45 +0200

    pr fixes

--- src/components/stats/statsQuerySrv.js
+++ src/components/stats/statsQuerySrv.js
@@ -33,40 +33,6 @@
                 };
             }
 
-            //StatsQuerySrv.getWeakestCategoryInLevel = function (level, optionalIds) {
-            //    var currWeakestCategory = {};
-            //
-            //    function _isOptional(categoryId) {
-            //        if (!angular.isArray(optionalIds)) {
-            //            return true;
-            //        }
-            //
-            //        return optionalIds.indexOf(categoryId) !== -1;
-            //    }
-            //
-            //    function _isMostWeakSoFar(categoryWeakness) {
-            //        return angular.isUndefined(currWeakestCategory.weakness) || currWeakestCategory.weakness < categoryWeakness;
-            //    }
-            //
-            //    return StatsSrv.getLevelStats(level).then(function (levelStats) {
-            //        angular.forEach(levelStats, function (categoryStats) {
-            //            var categoryWeakness = _getCategoryWeakness(categoryStats);
-            //
-            //            if (_isOptional(categoryStats.id) && _isMostWeakSoFar(categoryWeakness)) {
-            //                currWeakestCategory.weakness = categoryWeakness;
-            //                currWeakestCategory.category = angular.copy(categoryStats);
-            //                return;
-            //            }
-            //        });
-            //
-            //        if (currWeakestCategory.weakness === -Infinity) {
-            //            return null;
-            //        }
-            //
-            //        return currWeakestCategory.category;
-            //    });
-            //};
-
             StatsQuerySrv.getWeakestCategoryInLevel = function (level, optionalIds, parentId) {
                 function _isOptional(categoryStats) {
                     if (!optionalIds.length && angular.isUndefined(parentId)) {
