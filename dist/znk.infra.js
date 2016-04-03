0a381eab91f8b64eb30e2ee99e68ad21f549f4ec
Oded <oded.sharir@gmail.com>
2016-04-03 18:46:23 +0300

    change to showDoneButton + new build

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -4536,13 +4536,12 @@
                             return index && index === (questions.length - 1);
                         }
 
-                        function _getDoneBtnDisplayStatus() {
-                            var doneBtnStatus = false;
+                        function _determineDoneBtnDisplayStatus() {
                             var getQuestionsProm = znkExerciseDrvCtrl.getQuestions();
                             var areAllQuestionsAnsweredProm = znkExerciseDrvCtrl.areAllQuestionsAnswered();
-                            return $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
+                            $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
                                 if(isDoneBtnDisplayForced){
-                                    return doneBtnDisplayForcedValue;
+                                    return;
                                 }
                                 var questions = results[0];
                                 var areAllQuestionsAnswered = results[1];
@@ -4550,27 +4549,9 @@
                                 var currIndex = znkExerciseDrvCtrl.getCurrentIndex();
 
                                 if (_notReviewMode() && (_isLastQuestion(currIndex, questions) || areAllQuestionsAnswered)) {
-                                    doneBtnStatus = true;
-                                }
-
-                                return doneBtnStatus;
-                            });
-                        }
-
-                        function _determineDoneBtnDisplayStatus() {
-                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
-                                if (doneBtnStatus) {
                                     _setDoneBtnStatus(true);
                                 } else {
                                     _setDoneBtnStatus(false);
-                                }
-                            });
-                        }
-
-                        function _clickedEnterDoneBtn() {
-                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
-                                if (doneBtnStatus) {
-                                    scope.onDone();
                                 }
                             });
                         }
@@ -4602,12 +4583,10 @@
                         }
 
                         var isDoneBtnDisplayForced;
-                        var doneBtnDisplayForcedValue;
                         scope.actions.forceDoneBtnDisplay = function(display){
                             isDoneBtnDisplayForced = display === false || display === true;
 
                             if(isDoneBtnDisplayForced){
-                                doneBtnDisplayForcedValue = display;
                                 _setDoneBtnStatus(display);
                             }else{
                                 _determineDoneBtnDisplayStatus();
@@ -4675,7 +4654,9 @@
                                     scope.vm.nextQuestion();
                                     break;
                                 case ENTER_KEY:
-                                    _clickedEnterDoneBtn();
+                                    if(scope.vm.showDoneButton) {
+                                        scope.onDone();
+                                    }
                                     break;
                             }
                         }
--- src/components/znkExercise/core/directive/znkExerciseBtnSectionDrv.js
+++ src/components/znkExercise/core/directive/znkExerciseBtnSectionDrv.js
@@ -55,13 +55,12 @@
                             return index && index === (questions.length - 1);
                         }
 
-                        function _getDoneBtnDisplayStatus() {
-                            var doneBtnStatus = false;
+                        function _determineDoneBtnDisplayStatus() {
                             var getQuestionsProm = znkExerciseDrvCtrl.getQuestions();
                             var areAllQuestionsAnsweredProm = znkExerciseDrvCtrl.areAllQuestionsAnswered();
-                            return $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
+                            $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
                                 if(isDoneBtnDisplayForced){
-                                    return doneBtnDisplayForcedValue;
+                                    return;
                                 }
                                 var questions = results[0];
                                 var areAllQuestionsAnswered = results[1];
@@ -69,27 +68,9 @@
                                 var currIndex = znkExerciseDrvCtrl.getCurrentIndex();
 
                                 if (_notReviewMode() && (_isLastQuestion(currIndex, questions) || areAllQuestionsAnswered)) {
-                                    doneBtnStatus = true;
-                                }
-
-                                return doneBtnStatus;
-                            });
-                        }
-
-                        function _determineDoneBtnDisplayStatus() {
-                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
-                                if (doneBtnStatus) {
                                     _setDoneBtnStatus(true);
                                 } else {
                                     _setDoneBtnStatus(false);
-                                }
-                            });
-                        }
-
-                        function _clickedEnterDoneBtn() {
-                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
-                                if (doneBtnStatus) {
-                                    scope.onDone();
                                 }
                             });
                         }
@@ -121,12 +102,10 @@
                         }
 
                         var isDoneBtnDisplayForced;
-                        var doneBtnDisplayForcedValue;
                         scope.actions.forceDoneBtnDisplay = function(display){
                             isDoneBtnDisplayForced = display === false || display === true;
 
                             if(isDoneBtnDisplayForced){
-                                doneBtnDisplayForcedValue = display;
                                 _setDoneBtnStatus(display);
                             }else{
                                 _determineDoneBtnDisplayStatus();
@@ -194,7 +173,9 @@
                                     scope.vm.nextQuestion();
                                     break;
                                 case ENTER_KEY:
-                                    _clickedEnterDoneBtn();
+                                    if(scope.vm.showDoneButton) {
+                                        scope.onDone();
+                                    }
                                     break;
                             }
                         }
