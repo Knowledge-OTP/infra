157f8eafc72c8425a02fe1a955b9b5ff296148e3
Oded <oded.sharir@gmail.com>
2016-04-03 14:53:49 +0300

    add enter done button

--- src/components/znkExercise/core/directive/znkExerciseBtnSectionDrv.js
+++ src/components/znkExercise/core/directive/znkExerciseBtnSectionDrv.js
@@ -55,12 +55,13 @@
                             return index && index === (questions.length - 1);
                         }
 
-                        function _determineDoneBtnDisplayStatus() {
+                        function _getDoneBtnDisplayStatus() {
+                            var doneBtnStatus = false;
                             var getQuestionsProm = znkExerciseDrvCtrl.getQuestions();
                             var areAllQuestionsAnsweredProm = znkExerciseDrvCtrl.areAllQuestionsAnswered();
-                            $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
+                            return $q.all([getQuestionsProm, areAllQuestionsAnsweredProm]).then(function (results) {
                                 if(isDoneBtnDisplayForced){
-                                    return;
+                                    return doneBtnDisplayForcedValue;
                                 }
                                 var questions = results[0];
                                 var areAllQuestionsAnswered = results[1];
@@ -68,9 +69,27 @@
                                 var currIndex = znkExerciseDrvCtrl.getCurrentIndex();
 
                                 if (_notReviewMode() && (_isLastQuestion(currIndex, questions) || areAllQuestionsAnswered)) {
+                                    doneBtnStatus = true;
+                                }
+
+                                return doneBtnStatus;
+                            });
+                        }
+
+                        function _determineDoneBtnDisplayStatus() {
+                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
+                                if (doneBtnStatus) {
                                     _setDoneBtnStatus(true);
                                 } else {
                                     _setDoneBtnStatus(false);
+                                }
+                            });
+                        }
+
+                        function _clickedEnterDoneBtn() {
+                            _getDoneBtnDisplayStatus().then(function(doneBtnStatus) {
+                                if (doneBtnStatus) {
+                                    scope.onDone();
                                 }
                             });
                         }
@@ -102,10 +121,12 @@
                         }
 
                         var isDoneBtnDisplayForced;
+                        var doneBtnDisplayForcedValue;
                         scope.actions.forceDoneBtnDisplay = function(display){
                             isDoneBtnDisplayForced = display === false || display === true;
 
                             if(isDoneBtnDisplayForced){
+                                doneBtnDisplayForcedValue = display;
                                 _setDoneBtnStatus(display);
                             }else{
                                 _determineDoneBtnDisplayStatus();
@@ -163,6 +184,7 @@
                         function keyboardClickCB(e){
                             var LEFT_ARROW_KEY = 37;
                             var RIGHT_ARROW_KEY = 39;
+                            var ENTER_KEY = 13;
 
                             switch(e.keyCode){
                                 case LEFT_ARROW_KEY:
@@ -170,6 +192,9 @@
                                     break;
                                 case RIGHT_ARROW_KEY:
                                     scope.vm.nextQuestion();
+                                    break;
+                                case ENTER_KEY:
+                                    _clickedEnterDoneBtn();
                                     break;
                             }
                         }
