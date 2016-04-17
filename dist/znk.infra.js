7def6d85181a278d05e544408e2af5075784a6ea
igormas <igor.masliansky@gmail.com>
2016-04-17 18:42:42 +0300

    new structure for dist directory

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -22,6 +22,12 @@
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.config', []);
+})(angular);
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra.content', []);
 })(angular);
 (function (angular) {
@@ -76,12 +82,6 @@
     'use strict';
 
     angular.module('znk.infra.hint', ['znk.infra.config']);
-})(angular);
-
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra.config', []);
 })(angular);
 
 (function (angular) {
@@ -406,6 +406,55 @@ angular.module('znk.infra.analytics').run(['$templateCache', function($templateC
 
 
 angular.module('znk.infra.autofocus').run(['$templateCache', function($templateCache) {
+
+}]);
+
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.config').provider('InfraConfigSrv', [
+        function () {
+            var storageServiceName;
+            var userDataFn;
+            this.setStorageServiceName = function(_storageServiceName){
+                storageServiceName = _storageServiceName;
+            };
+
+            this.setUserDataFn = function(_userDataFn) {
+                userDataFn = _userDataFn;
+            };
+
+            this.$get = [
+                '$injector', '$log', '$q',
+                function ($injector, $log, $q) {
+                    var InfraConfigSrv = {};
+
+                    InfraConfigSrv.getStorageService = function(){
+                        if(!storageServiceName){
+                            $log.debug('InfraConfigSrv: storage service name was not defined');
+                            return;
+                        }
+                        return $injector.get(storageServiceName);
+                    };
+
+                    InfraConfigSrv.getUserData = function(){
+                        var userDataInjected;
+                        if(!userDataFn){
+                            $log.debug('InfraConfigSrv: auth fn name was not defined');
+                            return;
+                        }
+                        userDataInjected = $injector.invoke(userDataFn);
+                        return $q.when(userDataInjected);
+                    };
+
+                    return InfraConfigSrv;
+                }
+            ];
+        }
+    ]);
+})(angular);
+
+angular.module('znk.infra.config').run(['$templateCache', function($templateCache) {
 
 }]);
 
@@ -2602,55 +2651,6 @@ angular.module('znk.infra.general').run(['$templateCache', function($templateCac
 })(angular);
 
 angular.module('znk.infra.hint').run(['$templateCache', function($templateCache) {
-
-}]);
-
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra.config').provider('InfraConfigSrv', [
-        function () {
-            var storageServiceName;
-            var userDataFn;
-            this.setStorageServiceName = function(_storageServiceName){
-                storageServiceName = _storageServiceName;
-            };
-
-            this.setUserDataFn = function(_userDataFn) {
-                userDataFn = _userDataFn;
-            };
-
-            this.$get = [
-                '$injector', '$log', '$q',
-                function ($injector, $log, $q) {
-                    var InfraConfigSrv = {};
-
-                    InfraConfigSrv.getStorageService = function(){
-                        if(!storageServiceName){
-                            $log.debug('InfraConfigSrv: storage service name was not defined');
-                            return;
-                        }
-                        return $injector.get(storageServiceName);
-                    };
-
-                    InfraConfigSrv.getUserData = function(){
-                        var userDataInjected;
-                        if(!userDataFn){
-                            $log.debug('InfraConfigSrv: auth fn name was not defined');
-                            return;
-                        }
-                        userDataInjected = $injector.invoke(userDataFn);
-                        return $q.when(userDataInjected);
-                    };
-
-                    return InfraConfigSrv;
-                }
-            ];
-        }
-    ]);
-})(angular);
-
-angular.module('znk.infra.infraConfig').run(['$templateCache', function($templateCache) {
 
 }]);
 
