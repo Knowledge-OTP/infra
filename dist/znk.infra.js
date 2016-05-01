fa48e94a0e8a1ae1a179b1ee15fab1c9897ff4d8
Ran Shternin <ran.shternin@gmail.com>
2016-05-01 12:13:18 +0300

    Ran/device not supported (#137)
    * working on deviceNotSupported directive
    
    * - fixed gulpfile
    - added deviceNotSupported directive
    
    * name fix
    
    * - name fix
    - added dist folder to git
    
    * - added comment/documentation for the device-not-supported directive

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -39,6 +39,12 @@
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.deviceNotSupported', []);
+})(angular);
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra.enum', []);
 })(angular);
 (function (angular) {
@@ -859,6 +865,55 @@ angular.module('znk.infra.content').run(['$templateCache', function($templateCac
 
 angular.module('znk.infra.contentAvail').run(['$templateCache', function($templateCache) {
 
+}]);
+
+/**
+ * Device Not Supported
+ * This directive hides all content on the page and shows a message and an image
+ * Parameters:
+ * title
+ * subtitle
+ * image src to display
+ * by default the message will show when the screen width is 1024px or below, this can be overridden by css at the application level
+ */
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.deviceNotSupported').directive('deviceNotSupported', [
+        function () {
+            return {
+                templateUrl: 'components/deviceNotSupported/deviceNotSupported.template.html',
+                restrict: 'E',
+                scope: {
+                    title: '@',
+                    subTitle: '@',
+                    imageSrc: '@'
+                },
+                link: function (scope, element, attrs) {
+                    scope.imageSrc = attrs.imageSrc;
+                    scope.title = attrs.title;
+                    scope.subTitle = attrs.subTitle;
+
+                    scope.styleObj = {
+                        'background-image' : 'url(' + scope.imageSrc + ')'
+                    };
+                }
+            };
+        }
+    ]);
+})(angular);
+
+angular.module('znk.infra.deviceNotSupported').run(['$templateCache', function($templateCache) {
+  $templateCache.put("components/deviceNotSupported/deviceNotSupported.template.html",
+    "<div class=\"device-not-supported-inner\">\n" +
+    "    <h1>{{title}}</h1>\n" +
+    "    <h2>{{subTitle}}</h2>\n" +
+    "    <div class=\"image-container\"\n" +
+    "         ng-style=\"styleObj\">\n" +
+    "        <img ng-src=\"{{imageSrc}}\" alt=\"hidden\">\n" +
+    "    </div>\n" +
+    "</div>\n" +
+    "");
 }]);
 
 (function (angular) {
@@ -7475,6 +7530,7 @@ angular.module('znk.infra.znkTimeline').run(['$templateCache', function($templat
         'znk.infra.stats',
         'znk.infra.hint',
         'znk.infra.znkTimeline',
-        'znk.infra.analytics'
+        'znk.infra.analytics',
+        'znk.infra.deviceNotSupported'
     ]);
 })(angular);
---
+++ src/components/deviceNotSupported/deviceNotSupported.directive.js
@@ -0,0 +1,35 @@
+/**
+ * Device Not Supported
+ * This directive hides all content on the page and shows a message and an image
+ * Parameters:
+ * title
+ * subtitle
+ * image src to display
+ * by default the message will show when the screen width is 1024px or below, this can be overridden by css at the application level
+ */
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.deviceNotSupported').directive('deviceNotSupported', [
+        function () {
+            return {
+                templateUrl: 'components/deviceNotSupported/deviceNotSupported.template.html',
+                restrict: 'E',
+                scope: {
+                    title: '@',
+                    subTitle: '@',
+                    imageSrc: '@'
+                },
+                link: function (scope, element, attrs) {
+                    scope.imageSrc = attrs.imageSrc;
+                    scope.title = attrs.title;
+                    scope.subTitle = attrs.subTitle;
+
+                    scope.styleObj = {
+                        'background-image' : 'url(' + scope.imageSrc + ')'
+                    };
+                }
+            };
+        }
+    ]);
+})(angular);
