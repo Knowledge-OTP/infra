321f84540e83264baa747b751e3ff69d677f81e5
odeds <oded.sharir@gmail.com>
2016-05-25 17:20:44 +0300

    Oded/version 5 add func (#149)
    * add userProfile srv
    
    * add user to znk.infra
    
    * new build
    
    * add user dist
    
    * add mixins + new build
    
    * add userGoals + userSchools + fixed in userProfile
    
    * fixed annotion in userSchools
    
    * add mixins
    
    * changed userGoals to work with sat, needs to change to be dynamic
    
    * new build
    
    * new build
    
    * new build + moved userGoals + userSchools to infra web app

---
+++ src/components/user/services/userProfileSrv.js
@@ -0,0 +1,39 @@
+'use strict';
+
+angular.module('znk.infra.user').service('UserProfileService',
+    function (InfraConfigSrv, StorageSrv) {
+
+        var profilePath = StorageSrv.variables.appUserSpacePath + '/profile';
+
+        this.getProfile = function () {
+            return InfraConfigSrv.getGlobalStorage().then(function(globalStorage) {
+                return globalStorage.get(profilePath).then(function (profile) {
+                    if (profile && (angular.isDefined(profile.email) || angular.isDefined(profile.nickname))) {
+                        return profile;
+                    }
+                    return InfraConfigSrv.getUserData().then(function(authData) {
+                        var emailFromAuth = authData.password ? authData.password.email : '';
+                        var nickNameFromAuth = authData.auth ? authData.auth.name : emailFromAuth;
+
+                        if (!profile.email) {
+                            profile.email = emailFromAuth;
+                        }
+                        if (!profile.nickname) {
+                            profile.nickname = nickNameFromAuth;
+                        }
+                        if (!profile.createdTime) {
+                            profile.createdTime = StorageSrv.variables.currTimeStamp;
+                        }
+
+                        return globalStorage.set(profilePath, profile);
+                    });
+                });
+            });
+        };
+
+        this.setProfile = function (newProfile) {
+            return InfraConfigSrv.getGlobalStorage().then(function(globalStorage) {
+                return globalStorage.set(profilePath, newProfile);
+            });
+        };
+});
