b14ea1af89beee9ee7e8ee0bd63fa6938d26f792
igormas <igor.masliansky@gmail.com>
2016-05-26 19:28:07 +0300

    dist

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -285,16 +285,7 @@
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra.autofocus', ['znk.infra.enum', 'znk.infra.svgIcon'])
-        .config([
-        'SvgIconSrvProvider',
-        function (SvgIconSrvProvider) {
-            var svgMap = {
-                'clock-icon': 'components/general/svg/clock-icon.svg'
-            };
-            SvgIconSrvProvider.registerSvgSources(svgMap);
-        }]);
-
+    angular.module('znk.infra.autofocus', ['znk.infra.enum', 'znk.infra.svgIcon']);
 })(angular);
 
 (function (angular) {
