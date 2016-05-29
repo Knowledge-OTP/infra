bb93c71d0007ed4a7200c337840fa9667922924a
igormas <igor.masliansky@gmail.com>
2016-05-29 16:37:57 +0300

    userData

--- demoShared/infraSrvUserDataConfig.js
+++ demoShared/infraSrvUserDataConfig.js
@@ -2,8 +2,10 @@
     'use strict';
 
     angular.module('demo').config(function (InfraConfigSrvProvider) {
-        InfraConfigSrvProvider.setUserDataFn(function(){
-            return '21794e2b-3051-4016-8491-b3fe70e8212d';
+        InfraConfigSrvProvider.setUserDataFn(function () {
+            return {
+                uid: '21794e2b-3051-4016-8491-b3fe70e8212d'
+            };
         });
     });
 })(angular);
