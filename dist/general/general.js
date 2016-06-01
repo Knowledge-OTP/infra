5b26dd2872b6107cd04c1230d18eb7032cebc3ba
Oded <oded.sharir@gmail.com>
2016-06-01 11:54:13 +0300

    add cut string filter to general

--- dist/general/general.js
+++ dist/general/general.js
@@ -72,6 +72,38 @@
     }]);
 })(angular);
 
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.general').filter('cutString', function cutStringFilter() {
+        return function (str, length, onlyFullWords) {
+            length = +length;
+            if (!str || length <= 0) {
+                return '';
+            }
+            if (isNaN(length) || str.length < length) {
+                return str;
+            }
+            var words = str.split(' ');
+            var newStr = '';
+            if (onlyFullWords) {
+                for (var i = 0; i < words.length; i++) {
+                    if (newStr.length + words[i].length <= length) {
+                        newStr = newStr + words[i] + ' ';
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                newStr = str.substr(0, length);
+            }
+
+            return newStr + '...';
+        };
+    });
+})(angular);
+
+
 /**
  *  @directive subjectIdToAttrDrv
  *  This directive is an evolution of 'subjectIdToClassDrv'
--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -2398,6 +2398,38 @@ angular.module('znk.infra.exerciseUtility').service('CategoryService', function 
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.general').filter('cutString', function cutStringFilter() {
+        return function (str, length, onlyFullWords) {
+            length = +length;
+            if (!str || length <= 0) {
+                return '';
+            }
+            if (isNaN(length) || str.length < length) {
+                return str;
+            }
+            var words = str.split(' ');
+            var newStr = '';
+            if (onlyFullWords) {
+                for (var i = 0; i < words.length; i++) {
+                    if (newStr.length + words[i].length <= length) {
+                        newStr = newStr + words[i] + ' ';
+                    } else {
+                        break;
+                    }
+                }
+            } else {
+                newStr = str.substr(0, length);
+            }
+
+            return newStr + '...';
+        };
+    });
+})(angular);
+
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra.general', ['znk.infra.enum', 'znk.infra.svgIcon'])
         .config([
         'SvgIconSrvProvider',
