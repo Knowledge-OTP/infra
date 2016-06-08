0afc0b42c24b5139615d31306214c379fd266a1a
Allen Jacobson <allen@zinkerz.com>
2016-06-08 18:19:49 +0300

    hotfix

--- demoShared/storagesConfig.js
+++ demoShared/storagesConfig.js
@@ -1,26 +1,32 @@
 (function (angular) {
     'use strict';
 
-    angular.module('demo').config(function (InfraConfigSrvProvider, ENV) {
+    angular.module('demo').config(function (InfraConfigSrvProvider) {
         var authDbPath = 'https://znk-dev.firebaseio.com/';
         var dataDbPath = 'https://sat-dev.firebaseio.com/';
 
-        var authRef = new Firebase(authDbPath, ENV.firebaseAppScopeName);
-        var authProm = authRef.authWithPassword({
-            email: 'tester@zinkerz.com',
-            password: '111111'
-        }).then(function(res){
-            console.log('success', res);
-            return res;
-        }).catch(function(err){
-            console.error(err);
-        });
-
-        var dataRef = new Firebase(dataDbPath, ENV.firebaseAppScopeName);
-        var dataAuthProm = dataRef.authWithCustomToken('TykqAPXV4zlTTG0v6UuOt4OF3HssDykhJd90dAIc');
-
+        var authRef,
+            dataAuthProm,
+            authProm,
+            dataRef;
         function storageGetter(path) {
-            return function(storageFirebaseAdapter, StorageSrv, $q) {
+            return function(storageFirebaseAdapter, StorageSrv, $q, ENV) {
+                if(!authRef){
+                    authRef = new Firebase(authDbPath, ENV.firebaseAppScopeName);
+                    authProm = authRef.authWithPassword({
+                        email: 'tester@zinkerz.com',
+                        password: '111111'
+                    }).then(function(res){
+                        console.log('success', res);
+                        return res;
+                    }).catch(function(err){
+                        console.error(err);
+                    });
+                    dataRef = new Firebase(dataDbPath, ENV.firebaseAppScopeName);
+                    dataAuthProm = dataRef.authWithCustomToken('TykqAPXV4zlTTG0v6UuOt4OF3HssDykhJd90dAIc');
+                }
+
+
                 return $q.all([authProm, dataAuthProm]).then(function(res){
                     var auth = res[0];
                     var fbAdapter = storageFirebaseAdapter(path);
