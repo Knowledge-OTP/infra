a73658ab9ac6da8abaa44923b370e085c073901f
kosta <kosta.masliansky@gmail.com>
2016-06-09 16:29:32 +0300

    storage config through local storage

--- demoShared/storagesConfig.js
+++ demoShared/storagesConfig.js
@@ -9,13 +9,26 @@
             dataAuthProm,
             authProm,
             dataRef;
+
         function storageGetter(path) {
             return function(storageFirebaseAdapter, StorageSrv, $q, ENV) {
+                var APP_NAME_KEY = 'znkFbData';  // local storage keys;
+                var EMAIL_KEY = 'znkUser';
+                var PASSWORD_KEY = 'znkPwd';
+
+                var appName = _returnValOrDefault(localStorage.getItem(APP_NAME_KEY), ENV.firebaseAppScopeName);
+                var email = _returnValOrDefault(localStorage.getItem(EMAIL_KEY),  'tester@zinkerz.com');
+                var password = _returnValOrDefault(localStorage.getItem(PASSWORD_KEY), '111111');
+
+                function _returnValOrDefault(val, defaultVal){
+                    return angular.isDefined(val) ? val : defaultVal;
+                }
+
                 if(!authRef){
-                    authRef = new Firebase(authDbPath, ENV.firebaseAppScopeName);
+                    authRef = new Firebase(authDbPath, appName);
                     authProm = authRef.authWithPassword({
-                        email: 'tester@zinkerz.com',
-                        password: '111111'
+                        email: email,
+                        password: password
                     }).then(function(res){
                         console.log('success', res);
                         return res;
@@ -25,7 +38,6 @@
                     dataRef = new Firebase(dataDbPath, ENV.firebaseAppScopeName);
                     dataAuthProm = dataRef.authWithCustomToken('TykqAPXV4zlTTG0v6UuOt4OF3HssDykhJd90dAIc');
                 }
-
 
                 return $q.all([authProm, dataAuthProm]).then(function(res){
                     var auth = res[0];
@@ -42,7 +54,8 @@
                 });
             };
         }
-
+        debugger;
         InfraConfigSrvProvider.setStorages(storageGetter(dataDbPath), storageGetter(dataDbPath + '/sat_app'));
     });
 })(angular);
+
