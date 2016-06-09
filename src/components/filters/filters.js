128cb1de69a0428d6bf56cc126d851d5bed3967b
stormcrow85 <ran.shternin@gmail.com>
2016-06-09 12:13:08 +0300

    - added formatTime filter

---
+++ src/components/filters/filters.js
@@ -0,0 +1,40 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.filters', []);
+})(angular);
+'use strict';
+
+(function (angular) {
+    'use strict';
+    function formatTime() {
+        return function(time) {
+            var t = parseInt(time, 10);
+            var hours = parseInt(t / 3600, 10); t = t - (hours * 3600);
+            var minutes = parseInt(t / 60, 10); t = t - (minutes * 60);
+            var content = '';
+            if (hours) {
+                if (content) {
+                    content += ', ';
+                }
+                content += hours + 'h';
+            }
+            if (minutes > 0) {
+                if (content) {
+                    content += ', ' + minutes + 'm';
+                } else {
+                    content += minutes + 'm';
+                }
+            }
+            if (time < 60) content += t + ' sec';
+            return content;
+        };
+    }
+
+    angular.module('znk.infra.filters').filter('formatTime', formatTime);
+
+})(angular);
+
+angular.module('znk.infra.filters').run(['$templateCache', function($templateCache) {
+
+}]);
