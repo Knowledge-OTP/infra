1d7a748d8e704147b7fd0a1c49da6deeca5ec331
stormcrow85 <ran.shternin@gmail.com>
2016-06-13 17:26:41 +0300

    - working on formatDuration filter

--- dist/znk.infra.js
+++ dist/znk.infra.js
@@ -1990,22 +1990,27 @@ angular.module('znk.infra.exerciseResult').run(['$templateCache', function($temp
      * @param exp (expression to display time)
      * @returns formatted time string
      */
-    angular.module('znk.infra.filters').filter('formatDuration', function(){
+    angular.module('znk.infra.filters').filter('formatDuration', ['$log', function($log){
         return function(time, exp) {
-            var t = parseInt(time, 10);
-            var hours = parseInt(t / 3600, 10);
-            t = t - (hours * 3600);
-            var minutes = parseInt(t / 60, 10);
-            t = t - (minutes * 60);
-            var seconds =  Math.round(time % 60);
-            var defaultFormat = 'mm:ss';
+            if (angular.isNumber(time) && !isNaN(time)) {
+                var t = Math.round(parseInt(time));
+                var hours = parseInt(t / 3600, 10);
+                t = t - (hours * 3600);
+                var minutes = parseInt(t / 60, 10);
+                t = t - (minutes * 60);
+                var seconds =  time % 60;
+                var defaultFormat = 'mm:ss';
 
-            if (!exp) {
-                exp = defaultFormat;
+                if (!exp) {
+                    exp = defaultFormat;
+                }
+                return exp.replace(/hh/g,hours).replace(/mm/g,minutes).replace(/ss/g,seconds);
+            } else {
+                $log.error('time is not a number:', time);
+                return '';
             }
-            return exp.replace(/hh/g,hours).replace(/mm/g,minutes).replace(/ss/g,seconds);
         };
-    });
+    }]);
 })(angular);
 
 angular.module('znk.infra.filters').run(['$templateCache', function($templateCache) {
--- src/components/filters/formatDuration.filter.js
+++ src/components/filters/formatDuration.filter.js
@@ -5,20 +5,25 @@
      * @param exp (expression to display time)
      * @returns formatted time string
      */
-    angular.module('znk.infra.filters').filter('formatDuration', function(){
+    angular.module('znk.infra.filters').filter('formatDuration', ['$log', function($log){
         return function(time, exp) {
-            var t = Math.round(parseInt(time, 10));
-            var hours = parseInt(t / 3600, 10);
-            t = t - (hours * 3600);
-            var minutes = parseInt(t / 60, 10);
-            t = t - (minutes * 60);
-            var seconds =  time % 60;
-            var defaultFormat = 'mm:ss';
+            if (angular.isNumber(time) && !isNaN(time)) {
+                var t = Math.round(parseInt(time));
+                var hours = parseInt(t / 3600, 10);
+                t = t - (hours * 3600);
+                var minutes = parseInt(t / 60, 10);
+                t = t - (minutes * 60);
+                var seconds =  time % 60;
+                var defaultFormat = 'mm:ss';
 
-            if (!exp) {
-                exp = defaultFormat;
+                if (!exp) {
+                    exp = defaultFormat;
+                }
+                return exp.replace(/hh/g,hours).replace(/mm/g,minutes).replace(/ss/g,seconds);
+            } else {
+                $log.error('time is not a number:', time);
+                return '';
             }
-            return exp.replace(/hh/g,hours).replace(/mm/g,minutes).replace(/ss/g,seconds);
         };
-    });
+    }]);
 })(angular);
