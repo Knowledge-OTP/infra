30f4d5c3c83bd45374f4b0eff6b9f3ac4f3aba18
kosta <kosta.masliansky@gmail.com>
2016-06-13 19:01:38 +0300

    dist

--- dist/znkExercise/znkExercise.js
+++ dist/znkExercise/znkExercise.js
@@ -78,99 +78,6 @@
     ]);
 })(angular);
 
-/**
- * attrs:
- */
-
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra.znkExercise').directive('freeTextAnswer', ['ZnkExerciseViewModeEnum', '$timeout',
-
-        function (ZnkExerciseViewModeEnum, $timeout) {
-            return {
-                templateUrl: 'components/znkExercise/answerTypes/templates/freeTextAnswerDrv.html',
-                require: ['^ngModel', '^answerBuilder'],
-                scope:{},
-                link: function (scope, element, attrs, ctrls) {
-                    var ngModelCtrl = ctrls[0];
-                    var answerBuilderCtrl = ctrls[1];
-                    var userAnswerValidation = /^[0-9\/\.]{0,4}$/;
-
-                    scope.d = {};
-
-                    scope.d.userAnswer = '';  // stores the current userAnswer
-                    scope.d.userAnswerGetterSetter = function(newUserAnswer){
-                        if(arguments.length && _isAnswerValid(newUserAnswer)){
-                            scope.d.userAnswer = newUserAnswer;
-                            return scope.d.userAnswer;
-                        }
-                        return scope.d.userAnswer;
-                    };
-
-                    function _isAnswerValid(answerToCheck){
-                        return userAnswerValidation.test(answerToCheck);
-                    }
-
-                    var MODE_ANSWER_ONLY = ZnkExerciseViewModeEnum.ONLY_ANSWER.enum,
-                        MODE_REVIEW = ZnkExerciseViewModeEnum.REVIEW.enum,
-                        MODE_MUST_ANSWER = ZnkExerciseViewModeEnum.MUST_ANSWER.enum;
-
-                    scope.clickHandler = function(){
-                        ngModelCtrl.$setViewValue(scope.d.userAnswer);
-                        updateViewByCorrectAnswers(scope.d.userAnswer);
-                    };
-
-                    function updateViewByCorrectAnswers(userAnswer) {
-                        var correctAnswers = answerBuilderCtrl.question.correctAnswerText;
-                        var viewMode = answerBuilderCtrl.getViewMode();
-                        scope.correctAnswer = correctAnswers[0].content;
-
-                        if (viewMode === MODE_ANSWER_ONLY || viewMode === MODE_MUST_ANSWER) {
-                            scope.d.userAnswer = angular.isDefined(userAnswer) ? userAnswer : '';
-                            scope.showCorrectAnswer = false;
-                        } else {
-
-                            if (angular.isUndefined(userAnswer)) {
-                                // unanswered question
-                                    scope.userAnswerStatus = 'neutral';
-                                    scope.showCorrectAnswer = viewMode === MODE_REVIEW;
-                            } else {
-                                if (_isAnsweredCorrectly(userAnswer, correctAnswers)) {
-                                    scope.userAnswerStatus = 'correct';
-                                } else {
-                                    scope.userAnswerStatus = 'wrong';
-                                }
-                                scope.showCorrectAnswer = true;
-                                scope.d.userAnswer = userAnswer;
-                            }
-                        }
-                    }
-
-                    function _isAnsweredCorrectly(userAnswer,correctAnswers) {
-                        for (var i = 0; i < correctAnswers.length; i++) {
-                            if (userAnswer === correctAnswers[i].content) {
-                                return true;
-                            }
-                        }
-                        return false;
-                    }
-
-                    ngModelCtrl.$render = function () {
-                        //skip one digest cycle in order to let the answers time to be compiled
-                        $timeout(function(){
-                            updateViewByCorrectAnswers(ngModelCtrl.$viewValue);
-                        });
-                    };
-
-                    ngModelCtrl.$render();
-                }
-            };
-        }
-    ]);
-})(angular);
-
-
 
 /**
  * attrs:
@@ -2610,28 +2517,6 @@
 })(angular);
 
 angular.module('znk.infra.znkExercise').run(['$templateCache', function($templateCache) {
-  $templateCache.put("components/znkExercise/answerTypes/templates/freeTextAnswerDrv.html",
-    "<div class=\"free-text-answer-wrapper\" ng-switch=\"showCorrectAnswer\">\n" +
-    "\n" +
-    "    <div ng-switch-when=\"true\" class=\"answer-status-wrapper\" ng-class=\"userAnswerStatus\">\n" +
-    "        <div class=\"answer-status\">\n" +
-    "            <div class=\"user-answer\">{{d.userAnswer}}</div>\n" +
-    "            <svg-icon class=\"correct-icon\" name=\"correct\"></svg-icon>\n" +
-    "            <svg-icon class=\"wrong-icon\" name=\"wrong\"></svg-icon>\n" +
-    "        </div>\n" +
-    "        <div class=\"correct-answer\">Correct answer: <span>{{correctAnswer}}</span></div>\n" +
-    "    </div>\n" +
-    "\n" +
-    "    <div  ng-switch-when=\"false\" class=\"input-wrapper\">\n" +
-    "        <input ng-model-options=\"{ getterSetter: true }\" ng-model=\"d.userAnswerGetterSetter\">\n" +
-    "        <div class=\"arrow-wrapper\" ng-click=\"clickHandler()\">\n" +
-    "            <svg-icon name=\"arrow\"></svg-icon>\n" +
-    "            <div class=\"svg-back\"></div>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "\n" +
-    "</div>\n" +
-    "");
   $templateCache.put("components/znkExercise/answerTypes/templates/rateAnswerDrv.html",
     "<div class=\"rate-answer-wrapper\">\n" +
     "\n" +
