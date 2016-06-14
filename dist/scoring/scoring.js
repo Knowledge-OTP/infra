e294bc5d2e54035328daf5e1f0e1058cc708aaf1
Oded <oded.sharir@gmail.com>
2016-06-14 12:14:28 +0300

    new dist

--- dist/scoring/scoring.js
+++ dist/scoring/scoring.js
@@ -41,10 +41,14 @@ angular.module('znk.infra.scoring').provider('ScoringService', function() {
             });
         }
 
+        function _isShouldAddToScore(question) {
+            return (question.isAnsweredCorrectly && !question.afterAllowedTime);
+        }
+
         function _getRawScore(questionsResults) {
             var score = 0;
             angular.forEach(questionsResults, function (question) {
-                if (question.isAnsweredCorrectly) {
+                if (_isShouldAddToScore(question)) {
                     score += 1;
                 }
             });
--- dist/znkExercise/znkExercise.js
+++ dist/znkExercise/znkExercise.js
@@ -90,77 +90,125 @@
                 require: ['^ngModel', '^answerBuilder'],
                 scope:{},
                 link: function (scope, element, attrs, ctrls) {
-                    var ngModelCtrl = ctrls[0];
-                    var answerBuilderCtrl = ctrls[1];
-                    var userAnswerValidation = /^[0-9\/\.]{0,4}$/;
+                    var answerBuilder = ctrls[0];
+                    var ngModelCtrl = ctrls[1];
+                    var questionIndex = answerBuilder.question.__questionStatus.index;
+                    var currentSlide = answerBuilder.getCurrentIndex();    // current question/slide in the viewport
+                    var body = document.body;
+
+
+                    var MODE_ANSWER_WITH_QUESTION = ZnkExerciseViewModeEnum.ANSWER_WITH_RESULT.enum,
+                        MODE_ANSWER_ONLY = ZnkExerciseViewModeEnum.ONLY_ANSWER.enum,
+                        MODE_REVIEW = ZnkExerciseViewModeEnum.REVIEW.enum,
+                        MODE_MUST_ANSWER = ZnkExerciseViewModeEnum.MUST_ANSWER.enum;
+                    var keyMap = {};
 
                     scope.d = {};
 
-                    scope.d.userAnswer = '';  // stores the current userAnswer
-                    scope.d.userAnswerGetterSetter = function(newUserAnswer){
-                        if(arguments.length && _isAnswerValid(newUserAnswer)){
-                            scope.d.userAnswer = newUserAnswer;
-                            return scope.d.userAnswer;
+                    scope.d.answers = answerBuilder.question.answers;
+
+                    scope.d.click = function (answer) {
+                        var viewMode = answerBuilder.getViewMode();
+
+                        if ((!isNaN(parseInt(ngModelCtrl.$viewValue)) && viewMode === MODE_ANSWER_WITH_QUESTION) || viewMode === MODE_REVIEW) {
+                            return;
                         }
-                        return scope.d.userAnswer;
+                        ngModelCtrl.$setViewValue(answer.id);
+                        updateAnswersFollowingSelection(viewMode);
                     };
 
-                    function _isAnswerValid(answerToCheck){
-                        return userAnswerValidation.test(answerToCheck);
+                    function keyboardHandler(key){
+                        key = String.fromCharCode(key.keyCode).toUpperCase();
+                        if(angular.isDefined(keyMap[key])){
+                            scope.d.click(scope.d.answers[keyMap[key]]);
+                        }
                     }
 
-                    var MODE_ANSWER_ONLY = ZnkExerciseViewModeEnum.ONLY_ANSWER.enum,
-                        MODE_REVIEW = ZnkExerciseViewModeEnum.REVIEW.enum,
-                        MODE_MUST_ANSWER = ZnkExerciseViewModeEnum.MUST_ANSWER.enum;
+                    if(questionIndex === currentSlide){
+                        body.addEventListener('keydown',keyboardHandler);
+                    }
 
-                    scope.clickHandler = function(){
-                        ngModelCtrl.$setViewValue(scope.d.userAnswer);
-                        updateViewByCorrectAnswers(scope.d.userAnswer);
+                    scope.$on(ZnkExerciseEvents.QUESTION_CHANGED,function(event,value ,prevValue ,currQuestion){
+                        var currentSlide = currQuestion.__questionStatus.index;
+                        if(questionIndex !== currentSlide){
+                            body.removeEventListener('keydown',keyboardHandler);
+                        }else{
+                            body.addEventListener('keydown',keyboardHandler);
+                        }
+                    });
+
+
+
+                    scope.d.getIndexChar = function(answerIndex){
+                        var key = ZnkExerciseAnswersSrv.selectAnswer.getAnswerIndex(answerIndex,answerBuilder.question);
+                        keyMap[key] = answerIndex;
+                        return key;
                     };
 
-                    function updateViewByCorrectAnswers(userAnswer) {
-                        var correctAnswers = answerBuilderCtrl.question.correctAnswerText;
-                        var viewMode = answerBuilderCtrl.getViewMode();
-                        scope.correctAnswer = correctAnswers[0].content;
+                    function updateAnswersFollowingSelection(viewMode) {
+                        var selectedAnswerId = ngModelCtrl.$viewValue;
+                        var correctAnswerId = answerBuilder.question.correctAnswerId;
+                        var $answers = angular.element(element[0].querySelectorAll('.answer'));
+                        for (var i = 0; i < $answers.length; i++) {
 
-                        if (viewMode === MODE_ANSWER_ONLY || viewMode === MODE_MUST_ANSWER) {
-                            scope.d.userAnswer = angular.isDefined(userAnswer) ? userAnswer : '';
-                            scope.showCorrectAnswer = false;
-                        } else {
+                            var $answerElem = angular.element($answers[i]);
+                            if(!$answerElem || !$answerElem.scope || !$answerElem.scope()){
+                                continue;
+                            }
 
-                            if (angular.isUndefined(userAnswer)) {
-                                // unanswered question
-                                    scope.userAnswerStatus = 'neutral';
-                                    scope.showCorrectAnswer = viewMode === MODE_REVIEW;
+                            var answer = $answerElem.scope().answer;
+                            var classToAdd,
+                                classToRemove;
+
+                            if (answerBuilder.getViewMode() === MODE_ANSWER_ONLY || answerBuilder.getViewMode() === MODE_MUST_ANSWER) {
+                                // dont show correct / wrong indication
+                                classToRemove = 'answered';
+                                classToAdd = selectedAnswerId === answer.id ? 'answered' : 'neutral';
                             } else {
-                                if (_isAnsweredCorrectly(userAnswer, correctAnswers)) {
-                                    scope.userAnswerStatus = 'correct';
+                                // the rest of the optional states involve correct / wrong indications
+                                if (angular.isUndefined(selectedAnswerId)) {
+                                    // unanswered question
+                                    if (answerBuilder.getViewMode() === MODE_REVIEW) {
+                                        classToAdd = correctAnswerId === answer.id ? 'answered-incorrect' : 'neutral';
+                                    }
+                                } else if (selectedAnswerId === answer.id) {
+                                    // this is the selected answer
+                                    classToAdd = correctAnswerId === answer.id ? 'correct' : 'wrong';
                                 } else {
-                                    scope.userAnswerStatus = 'wrong';
+                                    // this is the correct answer but the user didn't select it
+                                    classToAdd = answer.id === correctAnswerId ? 'answered-incorrect' : 'neutral';
                                 }
-                                scope.showCorrectAnswer = true;
-                                scope.d.userAnswer = userAnswer;
                             }
-                        }
-                    }
+                            $answerElem.removeClass(classToRemove);
+                            $answerElem.addClass(classToAdd);
+                            if (viewMode === MODE_ANSWER_WITH_QUESTION){
+                                if (classToAdd === 'correct'){
 
-                    function _isAnsweredCorrectly(userAnswer,correctAnswers) {
-                        for (var i = 0; i < correctAnswers.length; i++) {
-                            if (userAnswer === correctAnswers[i].content) {
-                                return true;
+                                }
+                                if (classToAdd === 'wrong'){
+
+                                }
                             }
                         }
-                        return false;
                     }
 
                     ngModelCtrl.$render = function () {
                         //skip one digest cycle in order to let the answers time to be compiled
                         $timeout(function(){
-                            updateViewByCorrectAnswers(ngModelCtrl.$viewValue);
+                            updateAnswersFollowingSelection();
                         });
                     };
-
+                    //ng model controller render function not triggered in case render function was set
+                    // after the model value was changed
                     ngModelCtrl.$render();
+
+                    scope.$on('exercise:viewModeChanged', function () {
+                        ngModelCtrl.$render();
+                    });
+
+                    scope.$on('$destroy',function(){
+                        body.removeEventListener('keydown',keyboardHandler);
+                    });
                 }
             };
         }
