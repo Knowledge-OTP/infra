a42a1eb75cdad1c271fd045c651dafa982da03e8
kosta <kosta.masliansky@gmail.com>
2016-06-22 17:56:52 +0300

    znk audio

--- demo/znkAudioPlayer/demo.js
+++ demo/znkAudioPlayer/demo.js
@@ -11,7 +11,7 @@
         .controller('Main', function ($scope) {
             $scope.d = {};
             $scope.d.image = 'znkAudioPlayer.png';
-            $scope.d.audio = 'test.mp3';
+            $scope.d.audio = 'http://dfz02hjbsqn5e.cloudfront.net/toefl_app/audio/q_2185_gd_pa.mp3';
         });
 
 })(angular);
