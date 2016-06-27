ae6a94b9d6511854b08cc96f3f65935661686ffc
odeds <oded.sharir@gmail.com>
2016-06-27 16:48:31 +0300

    Oded/add ng inject (#182)
    * add ng inject
    
    * new dist
    
    * new dist
    
    * new dist

--- src/components/user/services/userProfileSrv.js
+++ src/components/user/services/userProfileSrv.js
@@ -2,7 +2,7 @@
 
 angular.module('znk.infra.user').service('UserProfileService',
     function (InfraConfigSrv, StorageSrv) {
-
+        'ngInject';
         var profilePath = StorageSrv.variables.appUserSpacePath + '/profile';
 
         this.getProfile = function () {
