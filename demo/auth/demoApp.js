508e92503383d3b51ad8c24bc0ac6e35f9ee14c6
kosta <kosta.masliansky@gmail.com>
2016-06-30 17:20:07 +0300

    in progress

--- demo/auth/demoApp.js
+++ demo/auth/demoApp.js
@@ -1,6 +1,17 @@
 (function (angular) {
     'use strict';
 
-    angular.module('demoApp', ['znk.infra.auth']);
+    angular.module('demo', ['znk.infra.auth', 'firebase'])
+        .controller('main', function (AuthService) {
+            var x;
+        })
+        .service('ENV', function () {
+            this.fbGlobalEndPoint = "https://znk-dev.firebaseio.com/";
+            this.backendEndpoint = "https://znk-web-backend-dev.azurewebsites.net/";
+            this.fbDataEndPoint = "https://act-dev.firebaseio.com/";
+            this.dataAuthSecret = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoicmFjY29vbnMifQ.mqdcwRt0W5v5QqfzVUBfUcQarD0IojEFNisP-SNIFLM";
+            this.redirectLogin = "http://dev-act.zinkerz.com.s3-website-eu-west-1.amazonaws.com/";
+            this.firebaseAppScopeName = "act_app";
+        })
 
-})(angular)
+})(angular);
--- src/components/auth/services/auth.service.js
+++ src/components/auth/services/auth.service.js
@@ -2,16 +2,31 @@
     'use strict';
 
     angular.module('znk.infra.auth').factory('AuthService',
-        function (ENV) {
+        function (ENV, $rootScope, $firebaseAuth, $window) {
             'ngInject';
 
-            var authService = {};
+            var refAuthDB = new $window.Firebase(ENV.fbGlobalEndPoint, ENV.firebaseAppScopeName);
+            //var refDataDB = new $window.Firebase(ENV.fbDataEndPoint, ENV.firebaseAppScopeName);
 
-            var rootRef = new Firebase(ENV.fbDataEndPoint, ENV.firebaseAppScopeName);
+            var authService = {};
+            var fbAuth = $firebaseAuth(refAuthDB);
+
+
 
             authService.getAuth = function(){
                 return rootRef.getAuth();
             };
+
+            authService.logout = function () {
+                debugger;
+                $rootScope.$broadcast('auth:beforeLogout');
+                fbAuth.$unauth();
+
+                //var actAuth = $firebaseAuth(refDataDB);
+                //actAuth.$unauth();
+            };
+            authService.logout();
+
 
             return authService;
         });
