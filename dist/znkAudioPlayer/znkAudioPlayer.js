4361803f4411135af1ffb6c127604720003f558b
kostamas <kosta.masliansky@gmail.com>
2016-07-05 16:23:07 +0300

    Kosta/znk audio player (#201)
    * on ended audio event handler
    
    * audio ended event
    
    * add show as done option
    
    * dist
    
    * without dist
    
    * showAsDone watcher
    
    * jshint
    
    *  dist
    
    * audio manager

--- dist/znkAudioPlayer/znkAudioPlayer.js
+++ dist/znkAudioPlayer/znkAudioPlayer.js
@@ -14,6 +14,41 @@
             }]);
 })(angular);
 
+'use strict';
+
+(function (angular) {
+
+    angular.module('znk.infra.znkAudioPlayer').directive('audioManager',
+        function () {
+            return {
+                require: 'audioManager',
+                controller: [
+                    '$scope', '$attrs',
+                    function ($scope, $attrs) {
+                        var resultData = $scope.$eval($attrs.audioManager);
+
+                        this.saveAsPlayedThrough = function saveAsPlayedThrough(groupData) {
+                            resultData.playedAudioArticles = resultData.playedAudioArticles || {};
+                            if (angular.isUndefined(resultData.playedAudioArticles[groupData.id])) {
+                                resultData.playedAudioArticles[groupData.id] = groupData.id;
+                                resultData.playedAudioArticles = angular.copy(resultData.playedAudioArticles);
+                                resultData.$save();
+                            }
+                        };
+
+                        this.wasPlayedThrough = function (groupData) {
+                            return !!resultData.playedAudioArticles && angular.isDefined(resultData.playedAudioArticles[groupData.id]);
+                        };
+
+                        this.canReplayAudio = function canReplayAudio() {
+                            return resultData.isComplete;
+                        };
+                    }]
+            };
+        });
+
+})(angular);
+
 
 (function (angular) {
     'use strict';
