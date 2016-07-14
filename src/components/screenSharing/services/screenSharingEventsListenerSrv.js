7029b405484d3db822fcfaaaa3abbe045e51da02
Igor <igor.masliansky@gmail.com>
2016-07-14 21:31:11 +0300

    saving work

---
+++ src/components/screenSharing/services/screenSharingEventsListenerSrv.js
@@ -0,0 +1,25 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.screenSharing').service('ScreenSharingEventsListenerSrv',
+        function (UserProfileService, InfraConfigSrv, $q, StorageSrv, ENV) {
+            'ngInject';
+
+            function _startListening(){
+                UserProfileService.getCurrUserId().then(function (currUid) {
+                    $q.when(InfraConfigSrv.getStorageService()).then(function(storage){
+                        var appName = ENV.firebaseAppScopeName;
+                        var userScreenSharingPath = appName + '/users/' + currUid +'/screenSharing';
+                        storage.onEvent(StorageSrv.EVENTS.VALUE, userScreenSharingPath, function(){
+
+                        });
+                    });
+                });
+            }
+
+            this.activate = function(){
+
+            };
+        }
+    );
+})(angular);
