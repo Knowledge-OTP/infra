59608b9f6b26298fdbcf707ba39d72b1231ed478
stormcrow85 <ran.shternin@gmail.com>
2016-07-26 21:29:15 +0300

    - working on call events

--- src/components/calls/services/callsEventsSrv.js
+++ src/components/calls/services/callsEventsSrv.js
@@ -8,7 +8,7 @@
             isEnabled = _isEnabled;
         };
 
-        this.$get = function (UserProfileService, InfraConfigSrv, $q, StorageSrv, ENV) {
+        this.$get = function (UserProfileService, InfraConfigSrv, $q, StorageSrv, ENV, CallsStatusEnum, CallsUiSrv) {
             'ngInject';
             var CallsEventsSrv = {};
 
@@ -16,25 +16,93 @@
                 var callsStatusPath = 'calls/' + guid;
 
                 function _cb(callsData) {
-                    if (!callsData || callsData.status !== CallsStatusEnum.CONFIRMED.enum) {
+
+                    if (!callsData) {
                         return;
                     }
 
                     UserProfileService.getCurrUserId().then(function (currUid) {
-                        var userCallsState = UserCallsStateEnum.NONE.enum;
+                        console.log('CallStatusEnum', CallsStatusEnum);
+                        console.log('callsData', callsData);
 
-                        if (callsData.viewerId === currUid) {
-                            userCallsState = UserCallsStateEnum.VIEWER.enum;
+                        switch(callsData.status) {
+                            case CallsStatusEnum.PENDING_CALL.enum:
+                                if (isCurrentUserInitiatedCall(currUid)) {
+                                    // show outgoing call modal
+                                    CallsUiSrv.showModal(CallsUiSrv.modals.OUTGOING_CALL, callsData);
+                                } else {
+                                    // show incoming call modal with the ACCEPT & DECLINE buttons
+                                    CallsUiSrv.showModal(CallsUiSrv.modals.INCOMING_CALL, callsData);
+                                }
+                                break;
+                            case CallsStatusEnum.DECLINE_CALL.enum:
+                                if (isCurrentUserInitiatedCall(currUid)) {
+                                    // show outgoing call modal WITH the DECLINED TEXT
+                                    CallsUiSrv.showModal(CallsUiSrv.modals.OUTGOING_CALL, callsData);
+                                } else {
+                                    // show incoming call modal WITH the DECLINED TEXT
+                                    CallsUiSrv.showModal(CallsUiSrv.modals.INCOMING_CALL, callsData);
+                                }
+                                break;
+                            case CallsStatusEnum.ACTIVE_CALL.enum:
+                                if (isCurrentUserInitiatedCall(currUid)) {
+                                    // show outgoing call modal WITH the ANSWERED TEXT, wait 2 seconds and close the modal, show the ActiveCallDRV
+                                    CallsUiSrv.showModal(CallsUiSrv.modals.OUTGOING_CALL, callsData);
+                                    CallsUiSrv.showActiveCallDrv();
+                                } else {
+                                    // close the modal, show the ActiveCallDRV
+                                    CallsUiSrv.closeModal();
+                                    CallsUiSrv.showActiveCallDrv();
+                                }
+                                break;
+                            case CallsStatusEnum.ENDED_CALL.enum:
+                                if (isCurrentUserInitiatedCall(currUid)) {
+                                    // hide the ActiveCallDRV
+                                } else {
+                                    // hide the ActiveCallDRV
+                                }
+                                CallsUiSrv.hideActiveCallDrv();
+                                // disconnect other user from call
+                                break;
                         }
 
-                        if (callsData.sharerId === currUid) {
-                            userCallsState = UserCallsStateEnum.SHARER.enum;
-                        }
 
-                        if (userCallsState !== UserCallsStateEnum.NONE.enum) {
-                            CallsSrv._userCallsStateChanged(userCallsState);
-                        }
+                        /**
+                         * Scenarios:
+                         * Call is pending, user initiated call
+                         * Call is pending, user receives call
+                         * Call declined, user initiated call
+                         * Call declined, user receives call
+                         * Call is Active,  user initiated call
+                         * Call is Active,  user receives call
+                         * Call ended, user initiated call
+                         * Call ended, user received call
+                         */
+
+                        // ['PENDING_CALL', 1, 'pending call'],
+                        // ['DECLINE_CALL', 2, 'decline call'],
+                        // ['ACTIVE_CALL', 3, 'active call'],
+                        // ['ENDED_CALL', 4, 'ended call']
+
+
+                        // var userCallState = UserCallStateEnum.NONE.enum;
+                        //
+                        // if (callsData.viewerId === currUid) {
+                        //     userCallState = UserCallStateEnum.VIEWER.enum;
+                        // }
+                        //
+                        // if (callsData.sharerId === currUid) {
+                        //     userCallState = UserCallStateEnum.SHARER.enum;
+                        // }
+                        //
+                        // if (userCallState !== UserCallStateEnum.NONE.enum) {
+                        //     CallsSrv._userCallStateChanged(userCallState);
+                        // }
                     });
+
+                    function isCurrentUserInitiatedCall(currUid) {
+                        return (currUid === callsData.callerId);
+                    }
                 }
 
                 InfraConfigSrv.getGlobalStorage().then(function (globalStorage) {
