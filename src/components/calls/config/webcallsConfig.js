ce341c0dccda5d6db024db40d173e76f49a07ae3
Oded <oded.sharir@gmail.com>
2016-07-26 21:44:06 +0300

    add setter data

--- src/components/calls/config/webcallsConfig.js
+++ src/components/calls/config/webcallsConfig.js
@@ -5,8 +5,8 @@
         .config(function (WebcallSrvProvider) {
             'ngInject';
             WebcallSrvProvider.setCallCred({
-                username:'assafshp160721153735',
-                password:'khjihghs'
+                username:'devUsrZinkerz160726161534',
+                password:'zinkerz$9999'
             });
         });
 })(angular);
--- src/components/calls/modals/controllers/IncomingCallModalCtrl.js
+++ src/components/calls/modals/controllers/IncomingCallModalCtrl.js
@@ -2,8 +2,26 @@
     'use strict';
 
     angular.module('znk.infra.calls').controller('IncomingCallModalCtrl', [
-        function () {
+        function (modalData, CallsSrv, $log) {
             'ngInject';
+
+            this.declineCall = function() {
+                var callsData = modalData.callsData;
+                CallsSrv.declineCall(callsData).then(function () {
+                    CallsUiSrv.closeModal();
+                }).catch(function (err) {
+                    $log.error('IncomingCallModalCtrl declineCall: err: ' + err);
+                });
+            };
+
+            this.acceptCall = function() {
+                var callsData = modalData.callsData;
+                CallsSrv.acceptCall(callsData).then(function () {
+                    CallsUiSrv.closeModal();
+                }).catch(function (err) {
+                    $log.error('IncomingCallModalCtrl acceptCall: err: ' + err);
+                });
+            };
         }]
     );
 })(angular);
