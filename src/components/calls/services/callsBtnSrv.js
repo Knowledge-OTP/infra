5845e20383d98de3c412911cafad5dc9d351277d
Oded <oded.sharir@gmail.com>
2016-08-01 13:21:40 +0300

    callerId cb

--- src/components/calls/services/callsBtnSrv.js
+++ src/components/calls/services/callsBtnSrv.js
@@ -36,6 +36,10 @@
                 angular.forEach(btnStatusCallbackMap[callsData.receiverId], function(cb) {
                     cb(status);
                 });
+
+                angular.forEach(btnStatusCallbackMap[callsData.callerId], function(cb) {
+                    cb(status);
+                });
             };
 
         });
