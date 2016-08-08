f6b92fcece01cc089fcea97101a16ec7f0bcda1a
orenor10 <orenor10@barak.net.il>
2016-08-08 20:00:10 +0300

    user assign module.contentAssign listener

--- dist/assignModule/assignModule.js
+++ dist/assignModule/assignModule.js
@@ -12,80 +12,11 @@
             var userAssignModuleService = {};
             userAssignModuleService.assignModules = {};
 
-            function getModuleSummary(assignModule) {
-                var exerciseId;
-                var exerciseTypeId = ExerciseTypeEnum.PRACTICE.enum,
-                    status = ExerciseStatusEnum.NEW.enum,
-                    correctAnswersNum = 0,
-                    wrongAnswersNum = 0,
-                    skippedAnswersNum = 0,
-                    totalAnswered = 0,
-                    duration = 0;
-
-                if (assignModule.exercises) {
-                    var practiceExercise = assignModule.exercises.filter(function (exercise) {
-                        return exercise.exerciseTypeId === exerciseTypeId ? exercise.exerciseId : null;
-                    });
-
-                    if (practiceExercise && practiceExercise.length) {
-                        exerciseId = practiceExercise[0].exerciseId;
-
-                        if (assignModule.exercisesStatus) {
-                            if (assignModule.exercisesStatus[exerciseTypeId] && assignModule.exercisesStatus[exerciseTypeId][exerciseId]) {
-                                status = assignModule.exercisesStatus[exerciseTypeId][exerciseId];
-                            }
-                        }
-
-                        if (assignModule.exerciseResults) {
-                            if (assignModule.exerciseResults[exerciseTypeId] && assignModule.exerciseResults[exerciseTypeId][exerciseId]) {
-                                correctAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].correctAnswersNum || 0;
-                                wrongAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].wrongAnswersNum || 0;
-                                skippedAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].skippedAnswersNum || 0;
-                                duration = assignModule.exerciseResults[exerciseTypeId][exerciseId].duration || 0;
-                                totalAnswered = correctAnswersNum + wrongAnswersNum;
-                            }
-                        }
-                    }
-                }
-
-                return {
-                    status: status,
-                    correctAnswersNum: correctAnswersNum,
-                    wrongAnswersNum: wrongAnswersNum,
-                    skippedAnswersNum: skippedAnswersNum,
-                    duration: duration,
-                    totalAnswered: totalAnswered
-                };
-            }
-
-            function onValueEventCB(userId, cb, moduleResultsGuids) {
-                if (angular.isUndefined(moduleResultsGuids)) {
-                    return;
-                }
-                var moduleResults = {};
-                var getProm = $q.when();
-                var getPromArr = [];
-                angular.forEach(moduleResultsGuids, function (resultGuid, moduleId) {
-                    getProm = ExerciseResultSrv.getModuleResult(userId, moduleId, false).then(function (moduleResult) {
-                        if (moduleResult && !angular.equals(moduleResult, {})) {
-                            moduleResults[moduleResult.moduleId] = moduleResult;
-                        }
-                    });
-                    getPromArr.push(getProm);
-                });
-
-                $q.all(getPromArr).then(function () {
-                    angular.forEach(moduleResults, function (assignModule) {
-                        assignModule.moduleSummary = getModuleSummary(assignModule);
-                    });
-
-                    userAssignModuleService.assignModules = moduleResults;
-
-                    if(angular.isFunction(cb)){
-                        cb(userAssignModuleService.assignModules);
-                    }
-                });
-            }
+            userAssignModuleService.assignModuleStatus = new EnumSrv.BaseEnum([
+                ['ASSIGNED', ExerciseStatusEnum.NEW.enum, 'assigned'],
+                ['IN-PROGRESS', ExerciseStatusEnum.ACTIVE.enum, 'in progress'],
+                ['COMPLETED', ExerciseStatusEnum.COMPLETED.enum, 'completed']
+            ]);
 
             userAssignModuleService.registerExternalOnValueCB = function (cb) {
                 UserProfileService.getCurrUserId().then(function (userId) {
@@ -93,7 +24,7 @@
                         return $q.when();
                     }
                     InfraConfigSrv.getStudentStorage().then(function (studentStorage) {
-                        studentStorage.onEvent('value', 'users/' + userId + '/moduleResults', onValueEventCB.bind(null, userId, cb));
+                        studentStorage.onEvent('value', 'users/' + userId + '/moduleResults', onValueEventCB.bind(null, userId, cb, studentStorage));
                     });
                 });
             };
@@ -150,11 +81,98 @@
                 });
             };
 
-            userAssignModuleService.assignModuleStatus = new EnumSrv.BaseEnum([
-                ['ASSIGNED', ExerciseStatusEnum.NEW.enum, 'assigned'],
-                ['IN-PROGRESS', ExerciseStatusEnum.ACTIVE.enum, 'in progress'],
-                ['COMPLETED', ExerciseStatusEnum.COMPLETED.enum, 'completed']
-            ]);
+            function getModuleSummary(assignModule) {
+                var exerciseId;
+                var exerciseTypeId = ExerciseTypeEnum.PRACTICE.enum,
+                    status = ExerciseStatusEnum.NEW.enum,
+                    correctAnswersNum = 0,
+                    wrongAnswersNum = 0,
+                    skippedAnswersNum = 0,
+                    totalAnswered = 0,
+                    duration = 0;
+
+                if (assignModule.exercises) {
+                    var practiceExercise = assignModule.exercises.filter(function (exercise) {
+                        return exercise.exerciseTypeId === exerciseTypeId ? exercise.exerciseId : null;
+                    });
+
+                    if (practiceExercise && practiceExercise.length) {
+                        exerciseId = practiceExercise[0].exerciseId;
+
+                        if (assignModule.exercisesStatus) {
+                            if (assignModule.exercisesStatus[exerciseTypeId] && assignModule.exercisesStatus[exerciseTypeId][exerciseId]) {
+                                status = assignModule.exercisesStatus[exerciseTypeId][exerciseId];
+                            }
+                        }
+
+                        if (assignModule.exerciseResults) {
+                            if (assignModule.exerciseResults[exerciseTypeId] && assignModule.exerciseResults[exerciseTypeId][exerciseId]) {
+                                correctAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].correctAnswersNum || 0;
+                                wrongAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].wrongAnswersNum || 0;
+                                skippedAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].skippedAnswersNum || 0;
+                                duration = assignModule.exerciseResults[exerciseTypeId][exerciseId].duration || 0;
+                                totalAnswered = correctAnswersNum + wrongAnswersNum;
+                            }
+                        }
+                    }
+                }
+
+                return {
+                    status: status,
+                    correctAnswersNum: correctAnswersNum,
+                    wrongAnswersNum: wrongAnswersNum,
+                    skippedAnswersNum: skippedAnswersNum,
+                    duration: duration,
+                    totalAnswered: totalAnswered
+                };
+            }
+
+            function onValueEventCB(userId, cb, studentStorage, moduleResultsGuids) {
+                if (angular.isUndefined(moduleResultsGuids)) {
+                    return;
+                }
+                var moduleResults = {};
+                var getProm = $q.when();
+                var getPromArr = [];
+                angular.forEach(moduleResultsGuids, function (resultGuid, moduleId) {
+                    getProm = ExerciseResultSrv.getModuleResult(userId, moduleId, false).then(function (moduleResult) {
+                        if (moduleResult && !angular.equals(moduleResult, {})) {
+                            moduleResults[moduleResult.moduleId] = moduleResult;
+                        }
+                    });
+                    getPromArr.push(getProm);
+                });
+
+                $q.all(getPromArr).then(function () {
+                    angular.forEach(moduleResults, function (assignModule) {
+                        assignModule.moduleSummary = getModuleSummary(assignModule);
+
+                        if (!assignModule.contentAssign) {
+                            var modulePath = 'moduleResults/' + assignModule.guid + '/contentAssign';
+                            studentStorage.onEvent('value', modulePath, onContentAssignCB.bind(null, assignModule, studentStorage, cb));
+                        }
+                    });
+
+                    userAssignModuleService.assignModules = moduleResults;
+                    applyCB(cb);
+                });
+            }
+
+            function onContentAssignCB(assignModule, studentStorage, cb, contentAssign) {
+                if (contentAssign) {
+                    var modulePath = 'moduleResults/' + assignModule.guid + '/contentAssign';
+                    studentStorage.offEvent('value', modulePath, onContentAssignCB);
+
+                    userAssignModuleService.assignModules[assignModule.moduleId].contentAssign = contentAssign;
+                    applyCB(cb);
+                }
+            }
+
+            function applyCB(cb) {
+                if (angular.isFunction(cb)) {
+                    cb(userAssignModuleService.assignModules);
+                }
+            }
 
             return userAssignModuleService;
         }
--- dist/main.js
+++ dist/main.js
@@ -399,80 +399,11 @@ angular.module('znk.infra.analytics').run(['$templateCache', function($templateC
             var userAssignModuleService = {};
             userAssignModuleService.assignModules = {};
 
-            function getModuleSummary(assignModule) {
-                var exerciseId;
-                var exerciseTypeId = ExerciseTypeEnum.PRACTICE.enum,
-                    status = ExerciseStatusEnum.NEW.enum,
-                    correctAnswersNum = 0,
-                    wrongAnswersNum = 0,
-                    skippedAnswersNum = 0,
-                    totalAnswered = 0,
-                    duration = 0;
-
-                if (assignModule.exercises) {
-                    var practiceExercise = assignModule.exercises.filter(function (exercise) {
-                        return exercise.exerciseTypeId === exerciseTypeId ? exercise.exerciseId : null;
-                    });
-
-                    if (practiceExercise && practiceExercise.length) {
-                        exerciseId = practiceExercise[0].exerciseId;
-
-                        if (assignModule.exercisesStatus) {
-                            if (assignModule.exercisesStatus[exerciseTypeId] && assignModule.exercisesStatus[exerciseTypeId][exerciseId]) {
-                                status = assignModule.exercisesStatus[exerciseTypeId][exerciseId];
-                            }
-                        }
-
-                        if (assignModule.exerciseResults) {
-                            if (assignModule.exerciseResults[exerciseTypeId] && assignModule.exerciseResults[exerciseTypeId][exerciseId]) {
-                                correctAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].correctAnswersNum || 0;
-                                wrongAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].wrongAnswersNum || 0;
-                                skippedAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].skippedAnswersNum || 0;
-                                duration = assignModule.exerciseResults[exerciseTypeId][exerciseId].duration || 0;
-                                totalAnswered = correctAnswersNum + wrongAnswersNum;
-                            }
-                        }
-                    }
-                }
-
-                return {
-                    status: status,
-                    correctAnswersNum: correctAnswersNum,
-                    wrongAnswersNum: wrongAnswersNum,
-                    skippedAnswersNum: skippedAnswersNum,
-                    duration: duration,
-                    totalAnswered: totalAnswered
-                };
-            }
-
-            function onValueEventCB(userId, cb, moduleResultsGuids) {
-                if (angular.isUndefined(moduleResultsGuids)) {
-                    return;
-                }
-                var moduleResults = {};
-                var getProm = $q.when();
-                var getPromArr = [];
-                angular.forEach(moduleResultsGuids, function (resultGuid, moduleId) {
-                    getProm = ExerciseResultSrv.getModuleResult(userId, moduleId, false).then(function (moduleResult) {
-                        if (moduleResult && !angular.equals(moduleResult, {})) {
-                            moduleResults[moduleResult.moduleId] = moduleResult;
-                        }
-                    });
-                    getPromArr.push(getProm);
-                });
-
-                $q.all(getPromArr).then(function () {
-                    angular.forEach(moduleResults, function (assignModule) {
-                        assignModule.moduleSummary = getModuleSummary(assignModule);
-                    });
-
-                    userAssignModuleService.assignModules = moduleResults;
-
-                    if(angular.isFunction(cb)){
-                        cb(userAssignModuleService.assignModules);
-                    }
-                });
-            }
+            userAssignModuleService.assignModuleStatus = new EnumSrv.BaseEnum([
+                ['ASSIGNED', ExerciseStatusEnum.NEW.enum, 'assigned'],
+                ['IN-PROGRESS', ExerciseStatusEnum.ACTIVE.enum, 'in progress'],
+                ['COMPLETED', ExerciseStatusEnum.COMPLETED.enum, 'completed']
+            ]);
 
             userAssignModuleService.registerExternalOnValueCB = function (cb) {
                 UserProfileService.getCurrUserId().then(function (userId) {
@@ -480,7 +411,7 @@ angular.module('znk.infra.analytics').run(['$templateCache', function($templateC
                         return $q.when();
                     }
                     InfraConfigSrv.getStudentStorage().then(function (studentStorage) {
-                        studentStorage.onEvent('value', 'users/' + userId + '/moduleResults', onValueEventCB.bind(null, userId, cb));
+                        studentStorage.onEvent('value', 'users/' + userId + '/moduleResults', onValueEventCB.bind(null, userId, cb, studentStorage));
                     });
                 });
             };
@@ -537,11 +468,98 @@ angular.module('znk.infra.analytics').run(['$templateCache', function($templateC
                 });
             };
 
-            userAssignModuleService.assignModuleStatus = new EnumSrv.BaseEnum([
-                ['ASSIGNED', ExerciseStatusEnum.NEW.enum, 'assigned'],
-                ['IN-PROGRESS', ExerciseStatusEnum.ACTIVE.enum, 'in progress'],
-                ['COMPLETED', ExerciseStatusEnum.COMPLETED.enum, 'completed']
-            ]);
+            function getModuleSummary(assignModule) {
+                var exerciseId;
+                var exerciseTypeId = ExerciseTypeEnum.PRACTICE.enum,
+                    status = ExerciseStatusEnum.NEW.enum,
+                    correctAnswersNum = 0,
+                    wrongAnswersNum = 0,
+                    skippedAnswersNum = 0,
+                    totalAnswered = 0,
+                    duration = 0;
+
+                if (assignModule.exercises) {
+                    var practiceExercise = assignModule.exercises.filter(function (exercise) {
+                        return exercise.exerciseTypeId === exerciseTypeId ? exercise.exerciseId : null;
+                    });
+
+                    if (practiceExercise && practiceExercise.length) {
+                        exerciseId = practiceExercise[0].exerciseId;
+
+                        if (assignModule.exercisesStatus) {
+                            if (assignModule.exercisesStatus[exerciseTypeId] && assignModule.exercisesStatus[exerciseTypeId][exerciseId]) {
+                                status = assignModule.exercisesStatus[exerciseTypeId][exerciseId];
+                            }
+                        }
+
+                        if (assignModule.exerciseResults) {
+                            if (assignModule.exerciseResults[exerciseTypeId] && assignModule.exerciseResults[exerciseTypeId][exerciseId]) {
+                                correctAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].correctAnswersNum || 0;
+                                wrongAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].wrongAnswersNum || 0;
+                                skippedAnswersNum = assignModule.exerciseResults[exerciseTypeId][exerciseId].skippedAnswersNum || 0;
+                                duration = assignModule.exerciseResults[exerciseTypeId][exerciseId].duration || 0;
+                                totalAnswered = correctAnswersNum + wrongAnswersNum;
+                            }
+                        }
+                    }
+                }
+
+                return {
+                    status: status,
+                    correctAnswersNum: correctAnswersNum,
+                    wrongAnswersNum: wrongAnswersNum,
+                    skippedAnswersNum: skippedAnswersNum,
+                    duration: duration,
+                    totalAnswered: totalAnswered
+                };
+            }
+
+            function onValueEventCB(userId, cb, studentStorage, moduleResultsGuids) {
+                if (angular.isUndefined(moduleResultsGuids)) {
+                    return;
+                }
+                var moduleResults = {};
+                var getProm = $q.when();
+                var getPromArr = [];
+                angular.forEach(moduleResultsGuids, function (resultGuid, moduleId) {
+                    getProm = ExerciseResultSrv.getModuleResult(userId, moduleId, false).then(function (moduleResult) {
+                        if (moduleResult && !angular.equals(moduleResult, {})) {
+                            moduleResults[moduleResult.moduleId] = moduleResult;
+                        }
+                    });
+                    getPromArr.push(getProm);
+                });
+
+                $q.all(getPromArr).then(function () {
+                    angular.forEach(moduleResults, function (assignModule) {
+                        assignModule.moduleSummary = getModuleSummary(assignModule);
+
+                        if (!assignModule.contentAssign) {
+                            var modulePath = 'moduleResults/' + assignModule.guid + '/contentAssign';
+                            studentStorage.onEvent('value', modulePath, onContentAssignCB.bind(null, assignModule, studentStorage, cb));
+                        }
+                    });
+
+                    userAssignModuleService.assignModules = moduleResults;
+                    applyCB(cb);
+                });
+            }
+
+            function onContentAssignCB(assignModule, studentStorage, cb, contentAssign) {
+                if (contentAssign) {
+                    var modulePath = 'moduleResults/' + assignModule.guid + '/contentAssign';
+                    studentStorage.offEvent('value', modulePath, onContentAssignCB);
+
+                    userAssignModuleService.assignModules[assignModule.moduleId].contentAssign = contentAssign;
+                    applyCB(cb);
+                }
+            }
+
+            function applyCB(cb) {
+                if (angular.isFunction(cb)) {
+                    cb(userAssignModuleService.assignModules);
+                }
+            }
 
             return userAssignModuleService;
         }
