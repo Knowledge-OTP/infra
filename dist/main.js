7e31073ab72de0d8d756cb1c4659a5e86ee9fa96
stormcrow85 <ran.shternin@gmail.com>
2016-08-15 11:49:46 +0300

    - added new service: teacherContextSrv - dist

--- dist/main.js
+++ dist/main.js
@@ -8475,6 +8475,48 @@ angular.module('znk.infra.user').run(['$templateCache', function($templateCache)
     ]);
 })(angular);
 
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra-dashboard.userContext').service('TeacherContextSrv', ['$window', '$log',
+
+        function ($window, $log) {
+            var TeacherContextSrv = {};
+
+            var _storageTeacherUidKey = 'currentTeacherUid';
+            var _currentTeacherUid = '';
+
+            TeacherContextSrv.getCurrUid = function () {
+                if (_currentTeacherUid.length === 0) {
+                    if ($window.sessionStorage) {
+                        var storedCurrentUid = $window.sessionStorage.getItem(_storageTeacherUidKey);
+                        if (storedCurrentUid) {
+                            _currentTeacherUid = storedCurrentUid;
+
+                        } else {
+                            $log.error('TeacherContextSrv: no teacher uid');
+                        }
+                    } else {
+                        $log.error('TeacherContextSrv: no teacher uid');
+                    }
+                }
+                return _currentTeacherUid;
+
+            };
+
+            TeacherContextSrv.setCurrentUid = function (uid) {
+                _currentTeacherUid = uid;
+
+                if ($window.sessionStorage) {
+                    $window.sessionStorage.setItem(_storageTeacherUidKey, uid);
+                }
+            };
+
+            return TeacherContextSrv;
+        }
+    ]);
+})(angular);
+
 angular.module('znk.infra.userContext').run(['$templateCache', function($templateCache) {
 
 }]);
