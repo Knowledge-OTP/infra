57e724c94000b8f12bf64e930cc8ec9a8a7e40d4
igormas <igor.masliansky@gmail.com>
2016-08-17 11:17:24 +0300

    saving work

--- demoShared/translate.config.js
+++ demoShared/translate.config.js
@@ -1,14 +1,20 @@
 (function (angular) {
     'use strict';
 
-    angular.module('demo').config(function($translateProvider){
-        $translateProvider.useLoader('$translatePartialLoader', {
-            urlTemplate: '/{part}/locale/{lang}.json'
-        }).preferredLanguage('en');
-    })
-    .run(function ($rootScope, $translate) {
-        $rootScope.$on('$translatePartialLoaderStructureChanged', function () {
-            $translate.refresh();
+    angular.module('demo')
+        .config(function ($translateProvider) {
+            $translateProvider.useLoader('$translatePartialLoader', {
+                urlTemplate: '/{part}/locale/{lang}.json'
+            }).preferredLanguage('en');
+        })
+        .run(function ($rootScope, $translate) {
+            $rootScope.$on('$translatePartialLoaderStructureChanged', function () {
+                $translate.refresh();
+            });
+        })
+        .run(function ($translatePartialLoader, $timeout) {
+            $timeout(function () {
+                $translatePartialLoader.addPart('demoLocale');
+            });
         });
-    });
 })(angular);
