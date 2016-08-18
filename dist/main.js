dd9572a3a5855a0067dd65bfe278c2a958caebf3
orenor10 <orenor10@barak.net.il>
2016-08-18 12:35:35 +0300

    add mail sender

--- dist/main.js
+++ dist/main.js
@@ -23,6 +23,7 @@
 "znk.infra.filters",
 "znk.infra.general",
 "znk.infra.hint",
+"znk.infra.mailSender",
 "znk.infra.personalization",
 "znk.infra.pngSequence",
 "znk.infra.popUp",
@@ -5770,6 +5771,49 @@ angular.module('znk.infra.general').run(['$templateCache', function($templateCac
 })(angular);
 
 angular.module('znk.infra.hint').run(['$templateCache', function($templateCache) {
+
+}]);
+
+(function (angular) {
+    'use strict';
+    angular.module('znk.infra.mailSender', []);
+})(angular);
+
+(function (angular) {
+    'use strict';
+    angular.module('znk.infra.mailSender').service('MailSenderServiceService', [
+        '$log', 'ENV', '$http', 'UserProfileService',
+        function ($log, ENV, $http, UserProfileService) {
+            var mailSenderService = {};
+            var backendUrl = ENV.backendEndpoint + 'mail';
+            var httpConfig = {
+                headers: 'application/json'
+            };
+
+            mailSenderService.postMailRequest = function (mailObject) {
+                return UserProfileService.getProfile().then(function (profile) {
+                    mailObject.uid = profile.uid;
+                    return $http.post(backendUrl, mailObject, httpConfig).then(
+                        function (response) {
+                            return {
+                                data: response.data
+                            };
+                        },
+                        function (error) {
+                            return {
+                                data: error.data
+                            };
+                        });
+                });
+            };
+
+            return mailSenderService;
+        }
+    ]);
+})(angular);
+
+
+angular.module('znk.infra.mailSender').run(['$templateCache', function($templateCache) {
 
 }]);
 
---
+++ src/components/mailSender/mailSender.service.js
@@ -0,0 +1,33 @@
+(function (angular) {
+    'use strict';
+    angular.module('znk.infra.mailSender').service('MailSenderServiceService', [
+        '$log', 'ENV', '$http', 'UserProfileService',
+        function ($log, ENV, $http, UserProfileService) {
+            var mailSenderService = {};
+            var backendUrl = ENV.backendEndpoint + 'mail';
+            var httpConfig = {
+                headers: 'application/json'
+            };
+
+            mailSenderService.postMailRequest = function (mailObject) {
+                return UserProfileService.getProfile().then(function (profile) {
+                    mailObject.uid = profile.uid;
+                    return $http.post(backendUrl, mailObject, httpConfig).then(
+                        function (response) {
+                            return {
+                                data: response.data
+                            };
+                        },
+                        function (error) {
+                            return {
+                                data: error.data
+                            };
+                        });
+                });
+            };
+
+            return mailSenderService;
+        }
+    ]);
+})(angular);
+
