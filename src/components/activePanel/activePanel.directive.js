bb5fff27821eb3424575c9b98607b09c1cc911ae
stormcrow85 <ran.shternin@gmail.com>
2016-08-22 14:30:37 +0300

    - working on activePanel DRV - dist

--- src/components/activePanel/activePanel.directive.js
+++ src/components/activePanel/activePanel.directive.js
@@ -65,7 +65,7 @@
                             SCREEN_SHARE_ACTIVE: 10,
                             BOTH_ACTIVE: 11
                         },
-                        showShareScreenBtns: true,
+                        shareScreenBtnsEnable: true,
                         isTeacher: isTeacher,
                         viewOtherUserScreen: function () {
                             var userData = {
@@ -125,88 +125,8 @@
                         }
                     };
 
-
-                    /**
-                     * From CallEventsSRV
-                     * @param component
-                     * @param status
-                     */
-                    // this.nonono_updateStatus = function (component, status) {
-                    //     if (!component || !status) {
-                    //         $log.error('must pass the component & status args to function');
-                    //         return;
-                    //     }
-                    //
-                    //     function isScreenSharingActive() {
-                    //         return (currentStatus.screenSharing === ActivePanelStatusEnum.ACTIVE.enum);
-                    //     }
-                    //
-                    //     function isCallActive() {
-                    //         return (currentStatus.calls === ActivePanelStatusEnum.ACTIVE.enum);
-                    //     }
-                    //
-                    //     switch (true) {
-                    //         case component === ActivePanelComponentEnum.CALLS.enum && status === ActivePanelStatusEnum.ACTIVE.enum :
-                    //             // component = call, status = active
-                    //             // show true
-                    //             // start timer
-                    //             // call btn in hangup mode
-                    //             currentStatus.calls = ActivePanelStatusEnum.ACTIVE.enum;
-                    //             actions.showUI();
-                    //             actions.startTimer();
-                    //             //callBtnMode('hangup');
-                    //             break;
-                    //
-                    //         case component === ActivePanelComponentEnum.CALLS.enum && status === ActivePanelStatusEnum.INACTIVE.enum :
-                    //             // component = call, status = inactive (hangup, disc')
-                    //             // actions.stopTimer
-                    //             // call btn is in call mode
-                    //             // if screenShare is inactive, hide drv
-                    //             currentStatus.calls = ActivePanelStatusEnum.INACTIVE.enum;
-                    //             actions.stopTimer();
-                    //             //callBtnMode('call');
-                    //             if (!isScreenSharingActive()) {
-                    //                 actions.hideUI();
-                    //             }
-                    //             break;
-                    //
-                    //         case component === ActivePanelComponentEnum.SCREEN_SHARE.enum && status === ActivePanelStatusEnum.ACTIVE.enum :
-                    //             // component = screenShare, status = active
-                    //             // show drv
-                    //             // screenShare buttons are disabled
-                    //             currentStatus.screenSharing = ActivePanelStatusEnum.ACTIVE.enum;
-                    //             actions.showUI();
-                    //             actions.screenShareMode(true);
-                    //             //screenShareBtnsMode('disabled');
-                    //             break;
-                    //
-                    //         case component === ActivePanelComponentEnum.SCREEN_SHARE.enum && status === ActivePanelStatusEnum.INACTIVE.enum :
-                    //             // component = screenShare, status = inactive
-                    //             // check if call is active, if not hide drv
-                    //             // return shareScreen btns to enabled state
-                    //             currentStatus.screenSharing = ActivePanelStatusEnum.INACTIVE.enum;
-                    //             if (!isCallActive()) {
-                    //                 //actions.hideUI(); // TODO: is this needed?
-                    //             }
-                    //             actions.screenShareMode(false);
-                    //             //screenShareBtnsMode('enabled');
-                    //             break;
-                    //
-                    //         default:
-                    //             actions.hideUI();
-                    //             break;
-                    //     }
-                    // };
-
                     function updateStatus() {
-                        // test:
-                        // callStatus = scope.d.states.CALL_ACTIVE;
-                        // callStatus = 0;
-                        // screenShareStatus = scope.d.states.SCREEN_SHARE_ACTIVE;
-                        // screenShareStatus = 0;
-
                         scope.d.currStatus = screenShareStatus + callStatus;
-                        // scope.d.currStatus = 1;
                         $log.debug('ActivePanel d.currStatus: ', scope.d.currStatus);
 
                         switch (scope.d.currStatus) {
@@ -223,14 +143,15 @@
                                 $log.debug('states.CALL_ACTIVE');
                                 break;
                             case scope.d.states.SCREEN_SHARE_ACTIVE :
-                                // component = screenShare, status = active
                                 // screenShare buttons are disabled
                                 actions.screenShareMode(true);
+                                scope.d.shareScreenBtnsEnable = false;
                                 //screenShareBtnsMode('disabled');
                                 $log.debug('states.SCREEN_SHARE_ACTIVE');
                                 break;
                             case scope.d.states.BOTH_ACTIVE :
                                 $log.debug('states.BOTH_ACTIVE');
+                                scope.d.shareScreenBtnsEnable = false;
                                 actions.screenShareMode(true);
                                 break;
 
