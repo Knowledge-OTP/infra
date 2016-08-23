300c2a8691f811ea27b7e884fb1acd8f9a15fa6c
Oded <oded.sharir@gmail.com>
2016-08-23 17:57:37 +0300

    add angularfire + auth change password

--- src/components/auth/services/auth.service.js
+++ src/components/auth/services/auth.service.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular.module('znk.infra.auth').factory('AuthService',
-        function (ENV) {
+        function (ENV, $q) {
             'ngInject';
 
             var refAuthDB = new Firebase(ENV.fbGlobalEndPoint, ENV.firebaseAppScopeName);
@@ -17,6 +17,15 @@
             authService.logout = function () {
                 refAuthDB.unauth();
                 rootRef.unauth();
+            };
+
+            authService.changePassword = function (changePasswordData) {
+                var refAuthData = refAuthDB.getAuth();
+                if (refAuthData && refAuthData.password) {
+                    changePasswordData.email = refAuthData.password.email;
+                    return refAuthData.$changePassword(changePasswordData);
+                }
+                return $q.reject('AuthService changePassword: user auth has no password in firebase!');
             };
 
             return authService;
