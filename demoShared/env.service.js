493fcab0c97d7a97eaf56b5de2a773c1d981063c
igormas <igor.masliansky@gmail.com>
2016-08-31 11:56:38 +0300

    saving work

--- demoShared/env.service.js
+++ demoShared/env.service.js
@@ -9,6 +9,7 @@
             this.dashboardAppName = 'sat_dashboard';
             this.videosEndPoint = '//dfz02hjbsqn5e.cloudfront.net/sat_app/';
             this.mediaEndPoint = '//dfz02hjbsqn5e.cloudfront.net/';
+            this.fbDataEndPoint = 'https://sat-dev.firebaseio.com/';
             this.fbGlobalEndPoint = 'https://znk-dev.firebaseio.com/';
         }
     );
