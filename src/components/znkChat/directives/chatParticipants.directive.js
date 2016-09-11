ae43b46e3183c0de102a029086afbe4bdf4ecf9e
kosta <kosta.masliansky@gmail.com>
2016-09-11 19:20:24 +0300

    pr fix

--- src/components/znkChat/directives/chatParticipants.directive.js
+++ src/components/znkChat/directives/chatParticipants.directive.js
@@ -14,15 +14,6 @@
                     scope.d = {};
                     scope.d.chatData = scope.chatData;
                     scope.d.selectChatter = scope.selectChatter();
-                    var ACTIVE_SUPPORT = -1;
-                    var FIRST_CHATTERS_ARRAY_INDEX = 0;
-
-                    if(scope.d.chatData.support && scope.d.chatData.support.uid){
-                        scope.d.selctedChatter = ACTIVE_SUPPORT;
-                    } else {
-                        scope.d.selctedChatter = FIRST_CHATTERS_ARRAY_INDEX;
-                    }
-
                 }
             };
         }
