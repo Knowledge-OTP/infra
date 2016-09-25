598db0758ca0f677f9fb070c43c7a746a8358c28
orenor10 <orenor10@barak.net.il>
2016-09-25 16:51:15 +0300

    svg icons

--- dist/auth/auth.js
+++ dist/auth/auth.js
@@ -10,7 +10,7 @@
     'use strict';
 
     angular.module('znk.infra.auth').factory('AuthService',
-        ["ENV", function (ENV) {
+        ["ENV", "$q", "$timeout", "$log", "StorageFirebaseAdapter", "StorageSrv", "$http", "$rootScope", function (ENV, $q, $timeout, $log, StorageFirebaseAdapter, StorageSrv, $http, $rootScope) {
             'ngInject';
 
             var refAuthDB = new Firebase(ENV.fbGlobalEndPoint, ENV.firebaseAppScopeName);
@@ -18,8 +18,59 @@
 
             var authService = {};
 
-            authService.getAuth = function() {
-                return rootRef.getAuth();
+            authService.saveRegistration = function (registration, login) {
+                var registerInProgress = true;
+                var dfd = $q.defer();
+                authService.logout(true);
+
+                var timeoutPromise = $timeout(function () {
+                    if (registerInProgress) {
+                        dfd.reject('timeout');
+                    }
+                }, ENV.promiseTimeOut);
+
+                registration.profile = {};
+
+                refAuthDB.createUser(registration).then(function () {
+                    registerInProgress = false;
+                    $timeout.cancel(timeoutPromise);
+
+                    if (login) {
+                        authService.login({
+                            email: registration.email,
+                            password: registration.password
+                        }).then(function (loginData) {
+                            authService.registerFirstLogin();
+                            dfd.resolve(loginData);
+                        }, function (err) {
+                            dfd.reject(err);
+                        });
+                    } else {
+                        dfd.resolve();
+                    }
+                }, function (error) {
+                    $timeout.cancel(timeoutPromise);
+                    dfd.reject(error);
+                });
+                return dfd.promise;
+            };
+
+            authService.login = function (loginData) {
+                var deferred = $q.defer();
+
+                refAuthDB.unauth();
+
+                refAuthDB.authWithPassword(loginData).then(function (authData) {
+                    $log.debug('authSrv::login(): uid=' + authData.uid);
+                    _onAuth(authData).then(function () {
+                        deferred.resolve(authData);
+                    });
+                }).catch(function (err) {
+                    authService.logout();
+                    deferred.reject(err);
+                });
+
+                return deferred.promise;
             };
 
             authService.logout = function () {
@@ -27,11 +78,98 @@
                 rootRef.unauth();
             };
 
+            authService.forgotPassword = function (forgotPasswordData) {
+                return refAuthDB.resetPassword(forgotPasswordData);
+            };
+
+            authService.getAuth = function() {
+                return rootRef.getAuth();
+            };
+
             authService.changePassword = function (changePasswordData) {
                 var refAuthData = refAuthDB.getAuth();
                 changePasswordData.email = (refAuthData.password && refAuthData.password.email) ? refAuthData.password.email : '';
                 return refAuthDB.changePassword(changePasswordData);
             };
+
+            authService.createAuthWithCustomToken = function (refDB, token) {
+                var deferred = $q.defer();
+                refDB.authWithCustomToken(token, function (error, userData) {
+                    if (error) {
+                        deferred.reject(error);
+                    }
+                    $log.debug('createAuthWithCustomToken: uid=' + userData.uid);
+                    deferred.resolve(userData);
+                });
+                return deferred.promise;
+            };
+
+            authService.userDataForAuthAndDataFb = function (data) {
+                var proms = [
+                    authService.createAuthWithCustomToken(refAuthDB, data.authToken),
+                    authService.createAuthWithCustomToken(rootRef, data.dataToken)
+                ];
+                return $q.all(proms);
+            };
+
+            authService.registerFirstLogin = function () {
+                var storageSrv = storageObj();
+                var firstLoginPath = 'firstLogin/' + authService.getAuth().uid;
+                return storageSrv.get(firstLoginPath).then(function (userFirstLoginTime) {
+                    if (angular.equals(userFirstLoginTime, {})) {
+                        storageSrv.set(firstLoginPath, Date.now());
+                    }
+                });
+            };
+
+            function storageObj (){
+                var fbAdapter = new StorageFirebaseAdapter(ENV.fbDataEndPoint + '/' + ENV.firebaseAppScopeName);
+                var config = {
+                    variables: {
+                        uid: function () {
+                            return authService.getAuth().uid;
+                        }
+                    }
+                };
+                return new StorageSrv(fbAdapter, config);
+            }
+
+            function _dataLogin() {
+                var postUrl = ENV.backendEndpoint + 'firebase/token';
+                var authData = refAuthDB.getAuth();
+                var postData = {
+                    email: authData.password ? authData.password.email : '',
+                    uid: authData.uid,
+                    fbDataEndPoint: ENV.fbDataEndPoint,
+                    fbEndpoint: ENV.fbGlobalEndPoint,
+                    auth: ENV.dataAuthSecret,
+                    token: authData.token
+                };
+
+                return $http.post(postUrl, postData).then(function (token) {
+                    var defer = $q.defer();
+                    rootRef.authWithCustomToken(token.data, function (error, userAuthData) {
+                        if (error) {
+                            defer.reject(error);
+                        }
+                        $log.debug('authSrv::login(): uid=' + userAuthData.uid);
+                        defer.resolve(userAuthData);
+                    });
+                    return defer.promise;
+                });
+            }
+
+            function _onAuth(data) {
+                var _loginAuthData = data;
+
+                if (_loginAuthData) {
+                    return _dataLogin(_loginAuthData).then(function () {
+                        $rootScope.$broadcast('auth:login', _loginAuthData);
+                    });
+                }
+                $rootScope.$broadcast('auth:logout');
+                return $q.when();
+            }
 
             return authService;
         }]);
