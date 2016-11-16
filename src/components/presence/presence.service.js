9601987ab89c5e98b167c5b13f6b85a78fde44c3
Oded <oded.sharir@gmail.com>
2016-11-16 11:01:45 +0200

    added fix for presence + new dist

--- src/components/presence/presence.service.js
+++ src/components/presence/presence.service.js
@@ -34,6 +34,15 @@
                             }
                         });
 
+                        // added listener for the user to resolve the problem when other tabs are closing
+                        // it removes user presence status, turning him offline, although his still online
+                        userRef.on('value', function(snapshot) {
+                            var val = snapshot.val();
+                            if (!val) {
+                                userRef.set(presenceService.userStatus.ONLINE);
+                            }
+                        });
+
                         $rootScope.$on('IdleStart', function() {
                             userRef.set(presenceService.userStatus.IDLE);
                         });
