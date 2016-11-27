696bc11ad01f6db7e9788b6cd5f78c6ed9dcf418
Ofir Nadav <ofir.nadav@gmail.com>
2016-11-27 15:02:42 +0200

    dist

--- dist/presence/presence.js
+++ dist/presence/presence.js
@@ -4,21 +4,21 @@
     angular.module('znk.infra.presence', [
         'ngIdle',
         'znk.infra.auth'
-    ]);
-        // .config(function (IdleProvider, KeepaliveProvider) {
-        //     // userIdleTime: how many sec until user is 'IDLE'
-        //     // idleTimeout: how many sec after idle to stop track the user, 0: keep track
-        //     // idleKeepalive: keepalive interval in sec
-        //
-        //     IdleProvider.idle(30);
-        //     IdleProvider.timeout(0);
-        //     KeepaliveProvider.interval(2);
-        // })
-        // .run(function (PresenceService, Idle) {
-        //         PresenceService.addCurrentUserListeners();
-        //         Idle.watch();
-        //     }
-        // );
+    ])
+        .config(function (IdleProvider, KeepaliveProvider) {
+            // userIdleTime: how many sec until user is 'IDLE'
+            // idleTimeout: how many sec after idle to stop track the user, 0: keep track
+            // idleKeepalive: keepalive interval in sec
+
+            IdleProvider.idle(30);
+            IdleProvider.timeout(0);
+            KeepaliveProvider.interval(2);
+        })
+        .run(function (PresenceService, Idle) {
+                PresenceService.addCurrentUserListeners();
+                Idle.watch();
+            }
+        );
 })(angular);
 
 'use strict';
