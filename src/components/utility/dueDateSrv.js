89298eeae19b2e353f490c7536f96f7ad1fabfd5
orenor10 <orenor10@barak.net.il>
2016-12-26 14:40:45 +0200

    homework changes

---
+++ src/components/utility/dueDateSrv.js
@@ -0,0 +1,22 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.utility').service('UtilitySrv', [function () {
+        this.isDueDatePass = function (dueDate) {
+            const daysInMs = 86400000;
+
+            if (angular.isUndefined(dueDate) || dueDate === null || dueDate === '') {
+                dueDate = 0;
+            }
+
+            var dateDiff = Math.abs(parseInt((Date.now() - dueDate) / daysInMs, 0));
+            var res = {
+                dateDiff: dateDiff,
+                passDue: (dateDiff > 0)
+            };
+
+            return res;
+        };
+    }
+    ]);
+})(angular);
