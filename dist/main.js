db93a05c72d8620b8b85f961ac1cab3c484c5f62
kosta mas <kosta.masliansky@gmail.com>
2017-01-23 21:56:34 +0200

    css fixes

--- dist/main.js
+++ dist/main.js
@@ -1253,7 +1253,7 @@ angular.module('znk.infra.autofocus').run(['$templateCache', function($templateC
             var svgMap = {
                 'incoming-call-icon': 'components/calls/svg/incoming-call-icon.svg',
                 'outgoing-call-icon': 'components/calls/svg/outgoing-call-icon.svg',
-                'call-error-exclamation-mark-icon': 'components/calls/svg/call-error-homework-exclamation-mark-icon.svg',
+                'call-error-exclamation-mark-icon': 'components/calls/svg/call-error-exclamation-mark-icon.svg',
                 'calls-etutoring-phone-icon': 'components/calls/svg/etutoring-phone-icon.svg'
             };
             SvgIconSrvProvider.registerSvgSources(svgMap);
