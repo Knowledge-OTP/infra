aa066cea9a1fe5a5d0d100373d680997bc8b14f4
Alex <choroshin@gmail.com>
2017-02-28 11:42:05 +0200

    add statsLevelEnum

--- dist/main.js
+++ dist/main.js
@@ -9531,6 +9531,22 @@ angular.module('znk.infra.sharedScss').run(['$templateCache', function($template
 (function (angular) {
     'use strict';
 
+    angular.module('znk.infra.stats').factory('statsLevelEnum', [
+        'EnumSrv',
+        function (EnumSrv) {
+            return new EnumSrv.BaseEnum([
+                ['LEVEL1', 1, 'level1Categories'],
+                ['LEVEL2', 2, 'level2Categories'],
+                ['LEVEL3', 3, 'level3Categories'],
+                ['LEVEL4', 4, 'level4Categories']
+            ]);
+        }
+    ]);
+})(angular);
+
+(function (angular) {
+    'use strict';
+
     angular.module('znk.infra.stats').service('StatsQuerySrv', [
         'StatsSrv', '$q',
         function (StatsSrv, $q) {
---
+++ src/components/stats/statsLevelEnum.js
@@ -0,0 +1,15 @@
+(function (angular) {
+    'use strict';
+
+    angular.module('znk.infra.stats').factory('statsLevelEnum', [
+        'EnumSrv',
+        function (EnumSrv) {
+            return new EnumSrv.BaseEnum([
+                ['LEVEL1', 1, 'level1Categories'],
+                ['LEVEL2', 2, 'level2Categories'],
+                ['LEVEL3', 3, 'level3Categories'],
+                ['LEVEL4', 4, 'level4Categories']
+            ]);
+        }
+    ]);
+})(angular);
