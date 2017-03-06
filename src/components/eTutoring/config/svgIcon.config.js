a9fdfe9df94e2042cd9f5684abcc7d74fa097cf7
allensy <allensy89@gmail.com>
2017-03-06 15:41:37 +0200

    save work

--- src/components/eTutoring/config/svgIcon.config.js
+++ src/components/eTutoring/config/svgIcon.config.js
@@ -1,13 +1,15 @@
 (function (angular) {
     'use strict';
 
-    angular.module('znk.infra.calls')
+    angular.module('znk.infra.eTutoring')
         .config(function (SvgIconSrvProvider) {
             'ngInject';
 
             var svgMap = {
                 'etutoring-exercise-icon': 'components/eTutoring/svg/etutoring-exercise-icon.svg',
-                'etutoring-slides-icon': 'components/eTutoring/svg/etutoring-slides-icon.svg'
+                'etutoring-slides-icon': 'components/eTutoring/svg/etutoring-slides-icon.svg',
+                'etutoring-calendar-icon': 'components/eTutoring/svg/etutoring-calendar-icon.svg',
+                'etutoring-close-icon': 'componets/eTutoring/svg/etutoring-close-popup.svg'
             };
             SvgIconSrvProvider.registerSvgSources(svgMap);
         });
