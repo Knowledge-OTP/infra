448d28431589497a9ff45ae26f3d03b2aaf7df9b
Ofir Nadav <ofir.nadav@gmail.com>
2017-04-06 18:31:41 +0300

    updateZnkCategoryStats

--- demo/znkCategoryStats/demo.js
+++ demo/znkCategoryStats/demo.js
@@ -6,6 +6,15 @@
         'pascalprecht.translate',
         'znk.infra.exerciseUtility'
     ])
+    .config(function () {
+            'ngInject';
+
+            localStorage.setItem('email','ofir+actStu11@zinkerz.com');
+            localStorage.setItem('password','123123');
+            localStorage.setItem('dataDbPath','https://act-dev.firebaseio.com/');
+            localStorage.setItem('studentPath','/act_app');
+            localStorage.setItem('teacherPath','/act_dashboard');
+    })
     .controller('Main', function (SubjectEnum) {
         'ngInject';
         var vm = this;
--- src/components/znkCategoryStats/znkCategoryStats.component.js
+++ src/components/znkCategoryStats/znkCategoryStats.component.js
@@ -24,11 +24,16 @@
                     vm.level1CategoryId = CategoryService.getCategoryLevel1ParentByIdSync(categoryId);
 
                     statsProm.then(function (categoryStats) {
+                        if (!categoryStats){
+                            categoryStats = 0;
+                        }
                         var extendObj = {};
                         extendObj.progress = getProgress(categoryStats);
                         extendObj.avgTime = getAvgTime(categoryStats);
 
                         vm.category = angular.extend(vm.category, extendObj);
+
+                        vm.category.specificArray = [{"id":"34","name":"Direct and inverse variation","levelProgress":0,"correct":0,"wrong":1,"totalQuestions":1},{"id":"43","name":"Qualitative Behavior of Functions","levelProgress":43,"correct":3,"wrong":4,"totalQuestions":7},{"id":"46","name":"Ratios and Proportions","levelProgress":25,"correct":2,"wrong":1,"totalQuestions":8},{"id":"50","name":"Mean/Median/Mode","levelProgress":18,"correct":3,"wrong":14,"totalQuestions":17},{"id":"51","name":"Complex Data interpretation","levelProgress":0,"correct":0,"wrong":5,"totalQuestions":6},{"id":"52","name":"Simple Data interpretation","levelProgress":14,"correct":4,"wrong":25,"totalQuestions":29},{"id":"58","name":"Percents","levelProgress":25,"correct":1,"wrong":2,"totalQuestions":4},{"id":"59","name":"Probability","levelProgress":0,"correct":0,"wrong":5,"totalQuestions":5},{"id":"60","name":"Statistical analysis, standard deviation","levelProgress":0,"correct":0,"wrong":3,"totalQuestions":3},{"id":"61","name":"Two-Way Tables","levelProgress":38,"correct":3,"wrong":5,"totalQuestions":8},{"id":"62","name":"Unit Conversion","levelProgress":17,"correct":1,"wrong":4,"totalQuestions":6}];
                     });
                 }
 
