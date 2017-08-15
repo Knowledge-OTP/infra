b4d66782e2912c6982716ef4dda882adebb2328d
orenor10 <orenor10@barak.net.il>
2017-08-15 20:27:27 +0300

    auth service

--- dist/sharedScss/sharedScss.js
+++
@@ -1,9 +0,0 @@
-(function (angular) {
-    'use strict';
-
-    angular.module('znk.infra.sharedScss', []);
-})(angular);
-
-angular.module('znk.infra.sharedScss').run(['$templateCache', function ($templateCache) {
-
-}]);
