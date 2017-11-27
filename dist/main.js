f818854a0a5e57907334449e72f94b6582f081b0
ofirnadav <ofir@zinkerz.com>
2017-11-28 01:16:25 +0200

    znkChat onDestroy fix

--- dist/main.js
+++ dist/main.js
@@ -13255,11 +13255,11 @@ angular.module('znk.infra.znkCategoryStats').run(['$templateCache', function ($t
                     var sound;
 
                     var audioLoadRetry = 1;
-                    
+
                     var audioSucessFn = function() {
                       audioLoadRetry = 1;
                     };
-                                
+
                     var audioStatusChangeFn = function(status) {
                       if (status === window.Media.MEDIA_STARTING && soundPlaying === true) {
                         sound.play();
@@ -13406,9 +13406,15 @@ angular.module('znk.infra.znkCategoryStats').run(['$templateCache', function ($t
                     }
 
                     scope.$on('$destroy', function () {
-                        znkChatEventSrv.offMsgOrNewChatEvent(offEvent.messageEvent.eventType, offEvent.messageEvent.path, offEvent.messageEvent.callback);
-                        znkChatEventSrv.offMsgOrNewChatEvent(offEvent.chatConnectionEvent.eventType, offEvent.chatConnectionEvent.path, offEvent.chatConnectionEvent.callback);
-                        PresenceService.stopTrackUserPresence(scope.chatterObj.uid);
+                        if (offEvent && offEvent.messageEvent) {
+                            znkChatEventSrv.offMsgOrNewChatEvent(offEvent.messageEvent.eventType, offEvent.messageEvent.path, offEvent.messageEvent.callback);
+                        }
+                        if (offEvent && offEvent.chatConnectionEvent) {
+                            znkChatEventSrv.offMsgOrNewChatEvent(offEvent.chatConnectionEvent.eventType, offEvent.chatConnectionEvent.path, offEvent.chatConnectionEvent.callback);
+                        }
+                        if (scope.chatterObj && scope.chatterObj.uid) {
+                            PresenceService.stopTrackUserPresence(scope.chatterObj.uid);
+                        }
                     });
                 }
             };
