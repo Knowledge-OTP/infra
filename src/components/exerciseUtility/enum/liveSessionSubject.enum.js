1d251ee543db68d2c175c7200a12d1bdac7d9193
OfirZinkerz <ofir@zinkerz.com>
2017-12-14 18:09:05 +0200

    LiveSessionSubjectEnumUpdate

--- src/components/exerciseUtility/enum/liveSessionSubject.enum.js
+++ src/components/exerciseUtility/enum/liveSessionSubject.enum.js
@@ -3,7 +3,8 @@
 
     var LiveSessionSubject = {
         MATH: 1,
-        ENGLISH: 2
+        ENGLISH: 2,
+        SCIENCE: 3
     };
 
     angular.module('znk.infra.exerciseUtility').constant('LiveSessionSubjectConst', LiveSessionSubject);
@@ -13,7 +14,8 @@
         function (EnumSrv) {
             return new EnumSrv.BaseEnum([
                 ['MATH', LiveSessionSubject.MATH, 'math'],
-                ['ENGLISH', LiveSessionSubject.ENGLISH, 'english']
+                ['ENGLISH', LiveSessionSubject.ENGLISH, 'english'],
+                ['SCIENCE', LiveSessionSubject.ENGLISH, 'science']
             ]);
         }
     ]);
