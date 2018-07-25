b0fa53c763c9666aebf68538cd93d6ab8aac1e58
arielw0 <ariel.weiss@gmail.com>
2018-07-25 12:11:21 +0300

    remove the diagnostic complete constraint from the live lessons tab

--- dist/eTutoring/eTutoring.js
+++ dist/eTutoring/eTutoring.js
@@ -628,7 +628,8 @@
             var SCRIPT_SRC = 'https://calendly.com/assets/external/widget.js';
 
             self.teachers = null;
-            $scope.diagnosticData = diagnosticData;
+            // Added "|| true" because we decided to remove the diagnostic complete constraint from the live lesson content
+            $scope.diagnosticData = diagnosticData || true;
             $scope.activeViewObj = {
                 view: +$stateParams.viewId || ETutoringViewsConst.LESSON
             };
